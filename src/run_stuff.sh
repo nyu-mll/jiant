d88e8cf62f2d007514a7a6306a2df91d3271aa3a
w4ngatang <wang.alex.c@gmail.com>
2017-12-15 17:10:10 -0500

    Add random seed as cmdline option

--- src/run_stuff.sh
+++ src/run_stuff.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # 
 #SBATCH -t 2-00:00
-#SBATCH --gres=gpu:p40:1
+#SBATCH --gres=gpu:1080ti:1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=aw3272@nyu.edu
 
 # TODO config files
 SCRATCH_PREFIX='/misc/vlgscratch4/BowmanGroup/awang/'
-SCRATCH_PREFIX='/beegfs/aw3272/'
+#SCRATCH_PREFIX='/beegfs/aw3272/'
 EXP_NAME=${3:-'debug'}
 GPUID=${2:-3}
+RANDOM_SEED=${17:-19}
 
 DATE=${16:-"$(date +%m-%d)"}
 LOG_PATH="${SCRATCH_PREFIX}/ckpts/$DATE/${EXP_NAME}/log.log"
@@ -52,5 +53,5 @@ N_EPOCHS=10
 TASK_ORDERING=${9:-small_to_large}
 
 CMD="python codebase/main.py --cuda ${GPUID} --log_file ${LOG_PATH}/${EXP_NAME}.log --tasks ${TASKS} --word_embs_file ${WORD_EMBS_FILE} --batch_size ${BATCH_SIZE} --lr ${LR}"
-ALLEN_CMD="python codebase/main_allen.py --cuda ${GPUID} --exp_name ${EXP_NAME} --log_file ${LOG_PATH} --exp_dir ${EXP_DIR} --tasks ${TASKS} --classifier ${CLASSIFIER} --vocab_path ${VOCAB_DIR} --max_vocab_size ${VOCAB_SIZE} --word_embs_file ${WORD_EMBS_FILE} --n_char_filters ${N_CHAR_FILTERS} --char_filter_sizes ${CHAR_FILTER_SIZES} --char_dim ${CHAR_DIM} --word_dim ${WORD_DIM} --hid_dim ${HID_DIM} --n_layers ${N_LAYERS} --pair_enc ${PAIR_ENC} --n_highway_layers ${N_HIGHWAY_LAYERS} --n_epochs ${N_EPOCHS} --batch_size ${BATCH_SIZE} --bpp_method ${BPP_METHOD} --bpp_base ${BPP_BASE} --optimizer ${OPTIMIZER} --lr ${LR} --lr_decay_factor ${LR_DECAY} --val_interval ${VAL_INTERVAL} --max_vals ${MAX_VALS} --task_ordering ${TASK_ORDERING} --scheduler_threshold ${SCHED_THRESH} --load_model ${LOAD_MODEL} --load_tasks ${LOAD_TASKS} --load_vocab ${LOAD_VOCAB} --load_index ${LOAD_INDEX} --should_train ${SHOULD_TRAIN}"
+ALLEN_CMD="python codebase/main_allen.py --cuda ${GPUID} --random_seed ${RANDOM_SEED} --exp_name ${EXP_NAME} --log_file ${LOG_PATH} --exp_dir ${EXP_DIR} --tasks ${TASKS} --classifier ${CLASSIFIER} --vocab_path ${VOCAB_DIR} --max_vocab_size ${VOCAB_SIZE} --word_embs_file ${WORD_EMBS_FILE} --n_char_filters ${N_CHAR_FILTERS} --char_filter_sizes ${CHAR_FILTER_SIZES} --char_dim ${CHAR_DIM} --word_dim ${WORD_DIM} --hid_dim ${HID_DIM} --n_layers ${N_LAYERS} --pair_enc ${PAIR_ENC} --n_highway_layers ${N_HIGHWAY_LAYERS} --n_epochs ${N_EPOCHS} --batch_size ${BATCH_SIZE} --bpp_method ${BPP_METHOD} --bpp_base ${BPP_BASE} --optimizer ${OPTIMIZER} --lr ${LR} --lr_decay_factor ${LR_DECAY} --val_interval ${VAL_INTERVAL} --max_vals ${MAX_VALS} --task_ordering ${TASK_ORDERING} --scheduler_threshold ${SCHED_THRESH} --load_model ${LOAD_MODEL} --load_tasks ${LOAD_TASKS} --load_vocab ${LOAD_VOCAB} --load_index ${LOAD_INDEX} --should_train ${SHOULD_TRAIN}"
 eval ${ALLEN_CMD}
--- src/submit_slurm.py
+++ src/submit_slurm.py
@@ -3,8 +3,9 @@
 import os
 import pdb
 import time
-import subprocess
+import random
 import datetime
+import subprocess
 
 ARG2IDX = {'tasks':1, # this is old
            'gpuid':2,
@@ -21,14 +22,15 @@ ARG2IDX = {'tasks':1, # this is old
            'HID_DIM':13,
            'LR':14,
            'PAIR_ENC':15,
-           'DATE':16
+           'DATE':16,
+           'RANDOM_SEED':17
           }
 
 DATE = datetime.datetime.now().strftime("%m-%d")
 DATE = "12-15"
 SCRATCH_PREFIX = '/misc/vlgscratch4/BowmanGroup/awang/ckpts/' + DATE + '/'
-SCRATCH_PREFIX = '/beegfs/aw3272/ckpts/' + DATE + '/'
-EXP_PREFIX = 'fixed'
+#SCRATCH_PREFIX = '/beegfs/aw3272/ckpts/' + DATE + '/'
+EXP_PREFIX = 'order'
 GPUID = str(0)
 SHOULD_TRAIN = str(1)
 LOAD_MODEL = str(0)
@@ -40,30 +42,31 @@ PAIR_TASKS = ['msrp', 'rte8', 'quora', 'snli', 'mnli', 'rte', 'sts-benchmark']
 SINGLE_TASKS = ['sst', 'twitter-irony']
 TASKS = PAIR_TASKS + SINGLE_TASKS
 HID_DIMS = [1024]
-BPP_METHOD = 'fixed'
-N_BPPS = [1]
+BPP_METHOD = 'percent_tr'
+N_BPPS = [100]
 BATS_BTW_VALS = [100, 1000, 5000]
 LRS = [1.]
 N_RUNS = 5
+ORDERS = ['large_to_small', 'random', 'random_per_pass']
 
 # Varying the validation metric for multi task
 for n_bpp in N_BPPS:
-    for bats_btw_val in BATS_BTW_VALS:
+    for run_idx in range(N_RUNS):
         for hid_dim in HID_DIMS:
-            for run_idx in range(N_RUNS):
+            for order in ORDERS:
                 lr = LRS[0]
-                exp_name = "%s_r%d_dim_%d_bbv_%d_lr_%.3f" % (EXP_PREFIX, run_idx, hid_dim, bats_btw_val, lr)
+                exp_name = "%s_%s_r%d_dim_%d" % (EXP_PREFIX, order, run_idx, hid_dim)
                 dir_name = SCRATCH_PREFIX + exp_name
                 out_file = dir_name + '/sbatch.out'
                 err_file = dir_name + '/sbatch.err'
                 if not os.path.exists(dir_name):
                     os.makedirs(dir_name)
-                #bats_btw_val = n_bpp
+                bats_btw_val = n_bpp
                 cmd = ["sbatch", "-J", exp_name, "-e", err_file, "-o", out_file,
                        "--mem=16GB",
                        "run_stuff.sh", ','.join(TASKS), GPUID, exp_name,
                        SHOULD_TRAIN, LOAD_MODEL, LOAD_TASKS, LOAD_VOCAB, LOAD_INDEX,
-                       'small_to_large', BPP_METHOD, str(n_bpp), str(bats_btw_val), str(hid_dim), str(lr),
+                       order, BPP_METHOD, str(n_bpp), str(bats_btw_val), str(hid_dim), str(lr),
                        'simple', DATE]
                 subprocess.call(cmd)
                 print(' '.join(cmd))
