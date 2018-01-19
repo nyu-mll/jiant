9a4540b682074d3f84a2fc232cf2a832dc96bce7
w4ngatang <wang.alex.c@gmail.com>
2018-01-19 11:51:19 -0500

    clean up formatting because i am a dum-dum

--- src/aggregate_results.py
+++ src/aggregate_results.py
@@ -6,11 +6,12 @@ from collections import defaultdict
 SCRATCH_PREFIX = '/misc/vlgscratch4/BowmanGroup/awang/ckpts/%s/%s/'
 SCRATCH_PREFIX = '/beegfs/aw3272/ckpts/%s/%s/'
 PROJECT_NAME = 'mtl-sent-rep'
-EXP_NAME = 'all_tasks_01_10_18'
+EXP_NAME = 'all_tasks_01_08_18'
 EXP_DIR = SCRATCH_PREFIX % (PROJECT_NAME, EXP_NAME)
 
-TASKS = ['mnli', 'snli', 'msrp', 'twitter-irony', 'sst', 'sts-benchmark',
-         'rte', 'rte5', 'rte8', 'quora']
+TASKS = ['mnli', 'snli', 'msrp', 'sst',
+         'twitter-irony', 'sts-benchmark', 'twitter', 'sts',
+         'rte', 'rte5', 'rte8', 'quora', 'micro', 'macro']
 
 OUT_FILE = "%s/summary.tex" % EXP_DIR
 
@@ -24,7 +25,10 @@ def extract_task(name):
     run_task = []
     for task in TASKS:
         if task in components:
-            run_task.append(task)
+            if '-' in task:
+                run_task.append(task.split('-')[0])
+            else:
+                run_task.append(task)
     try:
         assert len(run_task) == 1
     except:
@@ -85,22 +89,39 @@ def extract_results_one(log_file, task):
 
 def get_runs(exp_dir):
     runs = os.listdir(exp_dir)
-    #filtered_runs = [run for run in runs if 'attn' not in run and 'vocab' not in run]
-    filtered_runs = [run for run in runs if 'vocab' not in run and 'summary' not in run]
+    filtered_runs = [run for run in runs if 'attn' not in run and 'vocab' not in run and 'summary' not in run]
+    #filtered_runs = [run for run in runs if 'vocab' not in run and 'summary' not in run]
     return filtered_runs
 
 def latexify(out_file, results):
+
+    def get_orders(results):
+        run_order, metric_order = ['all'], ['macro', 'micro']
+        run_order += [k for k in results['valid'].keys() if k not in run_order]
+        metric_order += [k for k in results['valid'][run_order[0]].keys() if k not in metric_order]
+        task_order = [k for k in results['valid'][run_order[0]][metric_order[0]].keys()]
+        task_order.sort()
+        return run_order, metric_order, task_order
+
+
     average = lambda x: sum(x) / len(x)
     with open(out_file, 'w') as fh:
         header = 'run & metric & '
+        run_order, metric_order, task_order = get_orders(results)
+        header += ' & '.join(task_order) + ' \\\\\n'
+        header += '\\hline\n'
         fh.write(header)
-        for run_name in results['valid'].keys():
-            for metric, scores in results['valid'][run_name].items():
-                res_str = '%s & %s & ' % (run_name, metric)
-                metric_scores = [(k, v) for k, v in scores.items()]
-                metric_scores.sort(key=lambda x: x[0])
-                #res_str += '& '.join([average(scores[task]) for task in TASKS if task in scores])
-                res_str += ' & '.join(['%.3f' % average(scores[k]) for (k, _) in metric_scores])
+        for run_name in run_order:
+            for metric in metric_order:
+                if not len(results['valid'][run_name][metric]):
+                    continue
+                res_str = '%s & %s' % (run_name, metric)
+                scores = results['valid'][run_name][metric]
+                for task in task_order:
+                    try:
+                        res_str += ' & %.3f' % average(scores[task])
+                    except:
+                        pdb.set_trace()
                 res_str += ' \\\\\n'
                 fh.write(res_str)
 
@@ -117,18 +138,22 @@ for run_dir in get_runs(EXP_DIR):
         task = extract_task(run_dir)
         run_results = extract_results_one(os.path.join(EXP_DIR, run_dir, 'log.log'), task)
     else:
+        task = 'all'
         run_results = extract_results(os.path.join(EXP_DIR, run_dir, 'log.log'))
     run_name, run_idx = extract_name_and_run(run_dir)
     for metric, scores in run_results['valid'].items():
+        metric = extract_task(metric)
         for score, value in scores.items():
             if 'loss' in score:
                 continue
-            all_results['valid'][run_name][metric][score].append(value)
+            score = extract_task(score)
+            all_results['valid'][task][metric][score].append(value)
     for metric, value in run_results['test'].items():
         for score, value in scores.items():
             if 'loss' in score:
                 continue
-            all_results['test'][run_name][metric][score].append(value)
+            score = extract_task(score)
+            all_results['test'][task][metric][score].append(value)
 
 
 
