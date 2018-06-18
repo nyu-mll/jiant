64dd83c290400a2dfbd0c818d22e8099e595104e
Sam Bowman <sbowman@stanford.edu>
2018-06-18 16:11:04 -0400

    Saner default/example directory.

--- src/preprocess.py
+++ src/preprocess.py
@@ -22,7 +22,7 @@ if "cs.nyu.edu" in os.uname()[1] or "dgx" in os.uname()[1]:
     PATH_PREFIX = '/misc/vlgscratch4/BowmanGroup/awang/'
 else:
     PATH_PREFIX = '/beegfs/aw3272/'
-PATH_PREFIX = PATH_PREFIX + 'processed_data/mtl-sentence-representations/'
+PATH_PREFIX = PATH_PREFIX + 'glue_data/'
 
 NAME2INFO = {'sst': (SSTTask, 'SST-2/'),
              'cola': (CoLATask, 'CoLA/'),
