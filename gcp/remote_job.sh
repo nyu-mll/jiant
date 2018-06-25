6964323dd8b41aec388618f2358b8fd14b91ed3b
Ian Tenney <iftenney@gmail.com>
2018-06-25 00:28:19 +0000

    Add GCP helper scripts

---
+++ gcp/remote_job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Convenience script to execute a command (such as a training script) remotely 
+# on a worker instance. 
+
+# This is the simple version, which runs "synchronously": 
+# the current shell controls the process, and hitting Ctrl+C will kill the 
+# command (as will losing the SSH connection). 
+# For persistence, use remote_job_tmux.sh
+
+# Usage: ./remote_job.sh <instance_name> <command> [<zone>]
+INSTANCE_NAME="${1}"
+COMMAND="${2:-"nvidia-smi"}"
+ZONE="${3:-"us-east1-c"}"
+
+set -e
+
+if [ -z $INSTANCE_NAME ]; then
+  echo "You must provide an instance name!"
+  exit 1
+fi
+
+FULL_COMMAND="bash -l -c ${COMMAND}"
+
+set -x
+gcloud compute ssh --zone="$ZONE" "${INSTANCE_NAME}" \
+  --command="${FULL_COMMAND}"
+
+set +x
+echo "Remote command completed successfully."
+
---
+++ gcp/transfer_code.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Convenience script to copy this jiant repo to a (new) GCE instance.
+# Usage: ./transfer_code.sh <instance_name> [<zone>]
+INSTANCE_NAME="${1:-"$USER"}"
+ZONE="${2:-"us-east1-c"}"
+
+set -e
+
+if [ -z $INSTANCE_NAME ]; then
+  echo "You must provide an instance name!"
+  exit 1
+fi
+
+# Get parent dir of this script's dir
+JIANT_DIR=$(cd `dirname $0`/../; pwd)
+
+set -x
+gcloud compute scp --zone="$ZONE" --recurse $JIANT_DIR "$INSTANCE_NAME:~"
+
+echo "Transfer completed successfully."
