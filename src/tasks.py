b325caccfdc3db859bc2cd7a0e39c847df2f9859
roma-patel <romapatel@students-MacBook-Air.local>
2018-06-25 16:59:05 -0400

    added self defined metric for images

--- src/tasks.py
+++ src/tasks.py
@@ -608,13 +608,13 @@ class PDTBTask(PairClassificationTask):
         self.test_data_text = te_data
         log.info("\tFinished loading PDTB data.")
 
+
 class WeakGroundedTask(PairClassificationTask):
     ''' Task class for Weakly Grounded Sentences i.e., training on pairs of captions for the same image '''
 
-    def __init__(self, path, max_seq_len, name="mscoco"):
+    def __init__(self, path, max_seq_len, n_classes, name="weakgrounded"):
         ''' Do stuff '''
-        super(WeakGroundedTask, self).__init__(name, 3)
-
+        super(WeakGroundedTask, self).__init__(name, n_classes)
         self.load_data(path, max_seq_len)
         self.sentences = self.train_data_text[0] + self.train_data_text[1] + \
             self.val_data_text[0] + self.val_data_text[1]
@@ -623,42 +623,15 @@ class WeakGroundedTask(PairClassificationTask):
         ''' Process the dataset located at path.  '''
         ''' positive = captions of the same image, negative = captions of different images '''
         targ_map = {'negative': 0, 'positive': 1}
-    
+        targ_map = {'0': 0, '1': 1}
+
         tr_data = load_tsv(os.path.join(path, "train.tsv"), max_seq_len, targ_map=targ_map,
                            s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
-        val_data = load_tsv(os.path.join(path, "val.tsv"), max_seq_len, targ_map=targ_map,
+        # change to test for now!
+        val_data = load_tsv(os.path.join(path, "test.tsv"), max_seq_len, targ_map=targ_map,
                            s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
         # we don't have predefined test captions because this the ImageNet task, will probably add sampled sentences
-        te_data = load_tsv(os.path.join(path, "val.tsv"), max_seq_len, targ_map=targ_map,
-                           s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
-
-        self.train_data_text = tr_data
-        self.val_data_text = val_data
-        self.test_data_text = te_data
-        log.info("\tFinished loading MSCOCO data.")
-
-class WeakGroundedTask(PairClassificationTask):
-    ''' Task class for Weakly Grounded Sentences i.e., training on pairs of captions for the same image '''
-
-    def __init__(self, path, max_seq_len, name="mscoco"):
-        ''' Do stuff '''
-        super(WeakGroundedTask, self).__init__(name, 3)
-
-        self.load_data(path, max_seq_len)
-        self.sentences = self.train_data_text[0] + self.train_data_text[1] + \
-            self.val_data_text[0] + self.val_data_text[1]
-
-    def load_data(self, path, max_seq_len):
-        ''' Process the dataset located at path.  '''
-        ''' positive = captions of the same image, negative = captions of different images '''
-        targ_map = {'negative': 0, 'positive': 1}
-    
-        tr_data = load_tsv(os.path.join(path, "train.tsv"), max_seq_len, targ_map=targ_map,
-                           s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
-        val_data = load_tsv(os.path.join(path, "val.tsv"), max_seq_len, targ_map=targ_map,
-                           s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
-        # we don't have predefined test captions because this the ImageNet task, will probably add sampled sentences
-        te_data = load_tsv(os.path.join(path, "val.tsv"), max_seq_len, targ_map=targ_map,
+        te_data = load_tsv(os.path.join(path, "test.tsv"), max_seq_len, targ_map=targ_map,
                            s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
 
         self.train_data_text = tr_data
@@ -668,28 +641,41 @@ class WeakGroundedTask(PairClassificationTask):
 
 
 ''' change core task! '''       
-class GroundedTask(PairClassificationTask):
+class GroundedTask(Task):
     ''' Task class for Grounded Sentences i.e., training on caption->image pair '''
 
-    def __init__(self, path, max_seq_len, name="mscoco"):
+    def __init__(self, path, max_seq_len, name="grounded"):
         ''' Do stuff '''
-        super(WeakGroundedTask, self).__init__(name, 3)
-
+        super(GroundedTask, self).__init__(name)
+        self.scorer1 = Average()
+        self.scorer2 = None
+        self.val_metric = "%s_accuracy" % self.name
+        self.val_metric_decreases = False
         self.load_data(path, max_seq_len)
         self.sentences = self.train_data_text[0] + self.train_data_text[1] + \
             self.val_data_text[0] + self.val_data_text[1]
 
+    def get_metrics(self, reset=False):
+        '''Get metrics specific to the task'''
+        acc = self.scorer1.get_metric(reset)
+        return {'accuracy': acc}
+    
     def load_data(self, path, max_seq_len):
+        print('Inside load data!')
+        
         ''' Process the dataset located at path.  '''
         ''' positive = captions of the same image, negative = captions of different images '''
         targ_map = {'negative': 0, 'positive': 1}
-    
+        targ_map = {'0': 0, '1': 1}
+
         tr_data = load_tsv(os.path.join(path, "train.tsv"), max_seq_len, targ_map=targ_map,
                            s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
-        val_data = load_tsv(os.path.join(path, "val.tsv"), max_seq_len, targ_map=targ_map,
+
+        # change all to train for testing!
+        val_data = load_tsv(os.path.join(path, "test.tsv"), max_seq_len, targ_map=targ_map,
                            s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
         # we don't have predefined test captions because this the ImageNet task, will probably add sampled sentences
-        te_data = load_tsv(os.path.join(path, "val.tsv"), max_seq_len, targ_map=targ_map,
+        te_data = load_tsv(os.path.join(path, "test.tsv"), max_seq_len, targ_map=targ_map,
                            s1_idx=0, s2_idx=1, targ_idx=2, skip_rows=0)
 
         self.train_data_text = tr_data
