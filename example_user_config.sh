d4969d1ec443b4e7b3d1d007dd931c02e3921042
Sam Bowman <sbowman@stanford.edu>
2018-06-27 10:18:50 -0400

    Flesh out example.

--- example_user_config.sh
+++ example_user_config.sh
@@ -2,7 +2,7 @@
 # This is an example. Replace it with your own local setup,
 # remove the 'example' from the filename, and run experiments
 # with: 
-# source ../user_config.sh; python main.py --config ../config/demo.conf
+# source ../user_config.sh; python main.py --config ../config/demo.conf --overrides "do_train = 0"
 
 export JIANT_PROJECT_PREFIX=/Users/Bowman/Drive/JSALT
 export JIANT_DATA_DIR=/Users/Bowman/Drive/JSALT/jiant/glue_data
