ab905e67e990f4f619e56080cd88c0f50968980f
Sam Bowman <sbowman@stanford.edu>
2018-06-27 20:50:38 -0400

    LR changes.

--- major_experiment_scripts/early_tuning.sh
+++ major_experiment_scripts/early_tuning.sh
@@ -9,14 +9,13 @@ python main.py --config ../config/defaults.conf --overrides "run_name = base_tra
 python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_skip, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 1"
 
 
+python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_higher_lr, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, lr = 0.001"
+python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_high_lr, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, lr = 0.0003"
+python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_low_lr, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, lr = 0.00003"
 
-python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_high_lr, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, lr = 0.003"
-python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_low_lr, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, lr = 0.0003"
-python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_lower_lr, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, lr = 0.0001"
-
-python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_high_lr, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, lr = 0.003"
-python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_low_lr, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, lr = 0.0003"
-python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_lower_lr, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, lr = 0.0001"
+python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_higher_lr, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, lr = 0.001"
+python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_high_lr, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, lr = 0.0003"
+python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_low_lr, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, lr = 0.00003"
 
 python main.py --config ../config/defaults.conf --overrides "run_name = base_rnn_elmo, sent_enc = rnn, d_hid = 1500, n_layers_enc = 2, skip_embs = 0, elmo_chars_only = 0"
 python main.py --config ../config/defaults.conf --overrides "run_name = base_transformer_elmo, sent_enc = transformer, d_hid = 768, n_layers_enc = 12, skip_embs = 0, n_heads = 12, elmo_chars_only = 0"
