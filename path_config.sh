fabe6cd014b2d239fad7ebc60d0084e91e12bf38
Ian Tenney <iftenney@gmail.com>
2018-06-29 03:38:40 +0000

    Fix JIANT_PROJECT_PREFIX

--- path_config.sh
+++ path_config.sh
@@ -4,8 +4,10 @@
 # instructions below.
 
 # Copy this to /etc/profile.d/ to auto-set environment vars on login.
-# Or, customize this and run immediately before the training binary:
-# source ../path_config.sh; python main.py --config ../config/demo.conf \
+# Or, make a copy of this, customize, and run immediately before the training
+# binary:
+# cp path_config.sh ~/my_path_config.sh
+# source ~/my_path_config.sh; python main.py --config ../config/demo.conf \
 #   --overrides "do_train = 0"
 
 # Default environment variables for JSALT code. May be overwritten by user.
@@ -15,7 +17,7 @@ export JSALT_SHARE_DIR="/usr/share/jsalt"
 export JIANT_DATA_DIR="$JSALT_SHARE_DIR/glue_data"
 
 # Default experiment directory
-export JIANT_PROJECT_PREFIX="$JSALT_SHARE_DIR/exp"
+export JIANT_PROJECT_PREFIX="$HOME/exp"
 
 export GLOVE_EMBS_FILE="$JSALT_SHARE_DIR/glove/glove.840B.300d.txt"
 export FASTTEXT_EMBS_FILE="$JSALT_SHARE_DIR/fasttext/crawl-300d-2M.vec"
