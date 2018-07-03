9ab3269af577d4d99e94affbc7876fcedf9c267b
Sam Bowman <sbowman@stanford.edu>
2018-07-03 10:41:52 -0400

    Try/catch.

--- major_experiment_scripts/extract_results.py
+++ major_experiment_scripts/extract_results.py
@@ -16,56 +16,59 @@ today = datetime.datetime.now()
 # or if there is more junk after the eval line
 
 for path in sys.argv[1:]:
-  cols = {c : '' for c in col_order}
-  cols['date'] =  today.strftime("%m/%d/%Y")
-  results_line = None
-  found_eval = False
-  train_tasks = None
-  dropout = None
-  elmo = None
+  try:
+    cols = {c : '' for c in col_order}
+    cols['date'] =  today.strftime("%m/%d/%Y")
+    results_line = None
+    found_eval = False
+    train_tasks = None
+    dropout = None
+    elmo = None
 
-  with open(path) as f:
-    for line in f:
-      line = line.strip()
+    with open(path) as f:
+      for line in f:
+        line = line.strip()
 
-      if line == 'Evaluating...':
-        found_eval = True
-      else:
-        if found_eval:
-          assert (results_line is None), "Error! Multiple GLUE evals in this log\n"
-          results_line = line.strip()
-        found_eval = False
+        if line == 'Evaluating...':
+          found_eval = True
+        else:
+          if found_eval:
+            assert (results_line is None), "Error! Multiple GLUE evals in this log\n"
+            results_line = line.strip()
+          found_eval = False
 
-      train_m = re.match('Training model on tasks: (.*)', line)
-      if train_m:
-        task = train_m.groups()[0]
-        if train_tasks is not None:
-          assert (task == train_tasks), "Error! Multiple starts to training tasks, but tasks don't match: %s vs. %s"%(train_tasks, task)
-        train_tasks = task
+        train_m = re.match('Training model on tasks: (.*)', line)
+        if train_m:
+          task = train_m.groups()[0]
+          if train_tasks is not None:
+            assert (task == train_tasks), "Error! Multiple starts to training tasks, but tasks don't match: %s vs. %s"%(train_tasks, task)
+          train_tasks = task
 
-      do_m = re.match('"dropout": (.*),', line)
-      if do_m:
-        do = do_m.groups()[0]
-        if dropout is not None:
-          assert (dropout == do), "Error! Multiple dropouts set, but dropouts don't match: %s vs. %s"%(dropout, do)
-        dropout = do
+        do_m = re.match('"dropout": (.*),', line)
+        if do_m:
+          do = do_m.groups()[0]
+          if dropout is not None:
+            assert (dropout == do), "Error! Multiple dropouts set, but dropouts don't match: %s vs. %s"%(dropout, do)
+          dropout = do
 
-      el_m = re.match('"elmo_chars_only": (.*),', line)
-      if el_m:
-        el = el_m.groups()[0]
-        if elmo is not None:
-          assert (elmo == el), "Error! Multiple elmo flags set, but settings don't match: %s vs. %s"%(elmo, el)
-        elmo = el
+        el_m = re.match('"elmo_chars_only": (.*),', line)
+        if el_m:
+          el = el_m.groups()[0]
+          if elmo is not None:
+            assert (elmo == el), "Error! Multiple elmo flags set, but settings don't match: %s vs. %s"%(elmo, el)
+          elmo = el
 
-  cols['train_tasks'] = train_tasks
-  cols['dropout'] = dropout
-  cols['elmo'] = 'Y' if elmo == '0' else 'N'
+    cols['train_tasks'] = train_tasks
+    cols['dropout'] = dropout
+    cols['elmo'] = 'Y' if elmo == '0' else 'N'
 
-  for mv in results_line.strip().split(','):
-    metric, value = mv.split(':')
-    cols[metric.strip()] = '%.02f'%(100*float(value.strip()))
+    for mv in results_line.strip().split(','):
+      metric, value = mv.split(':')
+      cols[metric.strip()] = '%.02f'%(100*float(value.strip()))
 
-  print(path)
-  print('\t'.join(col_order))
-  print('\t'.join([cols[c] for c in col_order]))
+    print(path)
+    print('\t'.join(col_order))
+    print('\t'.join([cols[c] for c in col_order]))
+  except BaseException as e:
+    print("Error:", e)
 
