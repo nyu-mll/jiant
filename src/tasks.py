f1242573f8f79babdc627c08acde990e772d81ea
epavlick <elliepavlick@gmail.com>
2018-07-06 18:45:41 +0000

    fixing some messy merge

--- src/tasks.py
+++ src/tasks.py
@@ -1239,14 +1239,6 @@ class RecastNERTask(RecastNLITask):
     def __init__(self, path, max_seq_len, name="recast-ner"):
         super(RecastNERTask, self).__init__(name, 2)
 
-    def __init__(self, path, max_seq_len, name="recast-puns"):
-        super(RecastPunTask, self).__init__(name, 2)
-
-class RecastNERTask(RecastNLITask):
-
-    def __init__(self, path, max_seq_len, name="recast-ner"):
-        super(RecastNERTask, self).__init__(name, 2)
-
 class RecastVerbnetTask(RecastNLITask):
 
     def __init__(self, path, max_seq_len, name="recast-verbnet"):
