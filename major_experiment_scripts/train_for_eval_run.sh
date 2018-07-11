4eeaff8c1a70e7489cd6bf9697224378d38735af
yukatherin <yu.katherin@gmail.com>
2018-07-11 13:56:42 -0400

    set up shared conf and script for train_for_eval with existing

---
+++ major_experiment_scripts/train_for_eval_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PARAM_FILE="/nfs/jsalt/exp/katherin-worker6/main-wmt/noelmo/params.conf"
+MODEL_FILE="/nfs/jsalt/exp/katherin-worker6/main-wmt/noelmo/model_state_main_epoch_49.best_macro.th"
+
+EXP_NAME="main-wmt"
+RUN_NAME="noelmo_eval"
+
+
+python main.py --c config/defaults.conf,${PARAM_FILE},config/eval_existing.conf -o "load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, run_name = ${RUN_NAME}, eval_tasks = glue, train_for_eval = 1"
