8ea765f8ff8d8050a5ee9a40fae761aae82edb10
pappagari <raghu1991.p@gmail.com>
2018-07-12 13:49:58 +0000

    model importing was changed somehow by someone. Changing it back

--- src/models.py
+++ src/models.py
@@ -559,7 +559,7 @@ class MultiTaskModel(nn.Module):
         # feed forwarding inputs through sentence encoders
         sent1, mask1 = self.sent_encoder(batch['input1'])
         sent2, mask2 = self.sent_encoder(batch['input2'])
-        sent_pooler = self._get_classifier(self, task) # pooler for both Input and Response
+        sent_pooler = getattr(self, "%s_mdl" % task.name) # pooler for both Input and Response         
         sent_dnn = getattr(self, "%s_Response_mdl" % task.name) # dnn for Response
         sent1_rep = sent_pooler(sent1, mask1)
         sent2_rep_pool = sent_pooler(sent2, mask2)
