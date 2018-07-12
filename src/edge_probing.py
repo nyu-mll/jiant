24473480f44103af24db850386de43597d7d9fac
Ian Tenney <iftenney@gmail.com>
2018-07-12 23:36:17 +0000

    Refactor to use AllenNLP SpanExtractor utils

--- src/edge_probing.py
+++ src/edge_probing.py
@@ -9,7 +9,19 @@ from torch.autograd import Variable
 from .tasks import EdgeProbingTask
 from . import modules
 
+from allennlp.modules.span_extractors import \
+        EndpointSpanExtractor
+
 from typing import Dict
+
+
+def to_onehot(labels: torch.Tensor, n_classes: int):
+    """ Convert integer-valued labels to one-hot targets. """
+    binary_targets = torch.zeros([labels.shape[0], n_classes],
+                                 dtype=torch.int64, device=labels.device)
+    ridx = torch.arange(labels.shape[0], dtype=torch.int64)
+    binary_targets[ridx, labels] = 1
+    return binary_targets
 
 class EdgeClassifierModule(nn.Module):
     ''' Build edge classifier components as a sub-module.
@@ -28,14 +40,17 @@ class EdgeClassifierModule(nn.Module):
         self.span_enum_mode = task_params['span_enum_mode']
 
         proj_dim = task_params['d_hid']
-        # Build separate projection blocks, will apply once to sentence embeddings
-        # for each type (start1, end1, start2, end2).
-        self.proj_s1 = nn.Linear(d_inp, proj_dim)
-        self.proj_e1 = nn.Linear(d_inp, proj_dim)
-        self.proj_s2 = nn.Linear(d_inp, proj_dim)
-        self.proj_e2 = nn.Linear(d_inp, proj_dim)
-        # Classifier gets summed projections of (start1, end1, start2, end2)
-        self.classifier = modules.Classifier.from_params(proj_dim,
+        # Separate projection for span1, span2
+        self.proj1 = nn.Linear(d_inp, proj_dim)
+        self.proj2 = nn.Linear(d_inp, proj_dim)
+
+        # Span extractor, shared for both span1 and span2.
+        # Shouldn't actually have any parameters with the default config.
+        self.span_extractor = EndpointSpanExtractor(proj_dim, combination="x,y")
+
+        # Classifier gets summed projections of span1, span2
+        clf_input_dim = self.span_extractor.get_output_dim()  # 2 * proj_dim
+        self.classifier = modules.Classifier.from_params(clf_input_dim,
                                                          task.n_classes,
                                                          task_params)
 
@@ -49,8 +64,8 @@ class EdgeClassifierModule(nn.Module):
         Expects batch to have the following entries:
             'batch1' : [batch_size, max_len, ??]
             'labels' : [batch_size, num_targets] of label indices
-            'span1s' : pbatch)size, num_targets, 2] of spans
-            'span2s' : pbatch)size, num_targets, 2] of spans
+            'span1s' : [batch_size, num_targets, 2] of spans
+            'span2s' : [batch_size, num_targets, 2] of spans
 
         'labels', 'span1s', and 'span2s' are padded with -1 along second
         (num_targets) dimension.
@@ -67,72 +82,39 @@ class EdgeClassifierModule(nn.Module):
         """
         out = {}
 
-        # Apply projection layer to sent_embs.
-        # Get four different versions, which we can combine to span
-        # representations.
-        se_proj_s1 = self.proj_s1(sent_embs)
-        se_proj_e1 = self.proj_e1(sent_embs)
-        se_proj_s2 = self.proj_s2(sent_embs)
-        se_proj_e2 = self.proj_e2(sent_embs)
-
-        # For now, just extract specific spans and use a sigmoid loss.
-        # TODO to enumerate all spans and take the given targets as positives.
         batch_size = sent_embs.shape[0]
-        max_targets = batch['span1s'].shape[1]
-        mask = (batch['labels'] != -1)  # [batch_size, num_targets] bool
-        total_num_targets = mask.sum()
         out['n_inputs'] = batch_size
+
+        # Apply projection layers for each span.
+        se_proj1 = self.proj1(sent_embs)
+        se_proj2 = self.proj2(sent_embs)
+
+        # Span extraction.
+        #  span_mask = (batch['labels'] != -1)  # [batch_size, num_targets] bool
+        span_mask = (batch['span1s'][:,:,0] != -1)  # [batch_size, num_targets] bool
+        out['mask'] = span_mask
+        total_num_targets = span_mask.sum()
         out['n_targets'] = total_num_targets
         out['n_exs'] = total_num_targets  # used by trainer.py
-        # [batch_size, max_targets] of batch indices
-        batch_idxs = (torch.arange(batch_size, dtype=torch.int64)
-                      .repeat(max_targets, 1)
-                      .transpose(0,1))
-        # Get indices in preparation for a big gather.
-        flat_batch_idxs = batch_idxs[mask]   # [total_num_targets]
-        flat_span1s = batch['span1s'][mask]  # [total_num_targets, 2]
-        flat_span2s = batch['span2s'][mask]  # [total_num_targets, 2]
 
-        # RaSoR-style pooling: first and last token for each span.
-        # Results are [total_num_targets, repr_dim]
-        s1 = se_proj_s1[flat_batch_idxs, flat_span1s[:, 0]]
-        e1 = se_proj_e2[flat_batch_idxs, flat_span1s[:, 1]]
-        s2 = se_proj_s1[flat_batch_idxs, flat_span2s[:, 0]]
-        e2 = se_proj_e2[flat_batch_idxs, flat_span2s[:, 1]]
+        _kw = dict(sequence_mask=sent_mask.long(),
+                   span_indices_mask=span_mask.long())
+        def _extract_spans(embs, spans):
+            return self.span_extractor.forward(embs, spans, **_kw)
+        # span1_emb and span2_emb are [batch_size, num_targets, clf_input_dim]
+        span1_emb = _extract_spans(se_proj1, batch['span1s'])
+        span2_emb = _extract_spans(se_proj2, batch['span2s'])
 
-        # mimic first layer of a classifier by summing projected embs
-        span_vecs = s1 + e1 + s2 + e2  # [total_num_targets, proj_dim]
-        logits = self.classifier(span_vecs)  # [total_num_targets, n_classes]
+        # [batch_size, num_targets, n_classes]
+        logits = self.classifier(span1_emb + span2_emb)
         out['logits'] = logits
 
         # Compute loss if requested.
-        # TODO(iftenney): replace with sigmoid loss.
         if 'labels' in batch:
-            flat_labels = batch['labels'][mask]  # [total_num_targets]
-            task.acc_scorer(logits, flat_labels)
-
-            # binary cross-entropy against one-hot rows.
-            binary_targets = torch.zeros_like(logits, dtype=torch.float32)
-            ridx = torch.arange(flat_labels.shape[0], dtype=torch.int64)
-            # [total_num_targets, n_classes]
-            binary_targets[ridx, flat_labels] = 1
-
-            # needed for AllenNLP F1Measure()
-            # shape [total_num_targets, n_classes, 2], with float scores
-            binary_preds = torch.stack([-1*logits, logits], dim=2)
-            task.f1_scorer(binary_preds, binary_targets)
-            # Matthews coefficient computed on {0,1} labels.
-            task.mcc_scorer(logits.ge(0).long(), binary_targets.long())
-
-            if self.loss_type == 'softmax':
-                # softmax over each row.
-                out['loss'] = F.cross_entropy(logits, flat_labels)
-            elif self.loss_type == 'sigmoid':
-                out['loss'] = F.binary_cross_entropy(F.sigmoid(logits),
-                                                     binary_targets)
-            else:
-                raise ValueError("Unsupported loss type '%s' "
-                                 "for edge probing." % loss_type)
+            # Flatten to [total_num_targets, ...] first.
+            out['loss'] = self.compute_loss(logits[span_mask],
+                                            batch['labels'][span_mask],
+                                            task)
 
         if predict:
             # return argmax predictions
@@ -141,3 +123,41 @@ class EdgeClassifierModule(nn.Module):
         return out
 
 
+    def compute_loss(self, logits: torch.Tensor,
+                     labels: torch.Tensor, task: EdgeProbingTask):
+        """ Compute loss & eval metrics.
+
+        Expect logits and labels to be already "selected" for good targets,
+        i.e. this function does not do any masking internally.
+
+        Args:
+            logits: [total_num_targets, n_classes] Tensor of float scores
+            labels: [total_num_targets] Tensor of int targets
+
+        Returns:
+            loss: scalar Tensor
+        """
+        # Accuracy scorer can handle multiclass natively.
+        task.acc_scorer(logits, labels)
+
+        # Matthews and F1 need binary targets, as does sigmoid loss.
+        # [total_num_targets, n_classes] LongTensor
+        binary_targets = to_onehot(labels, n_classes=logits.shape[1])
+
+        # Matthews coefficient computed on {0,1} labels.
+        task.mcc_scorer(logits.ge(0).long(), binary_targets)
+
+        # F1Measure() expects [total_num_targets, n_classes, 2]
+        # to compute binarized F1.
+        binary_scores = torch.stack([-1*logits, logits], dim=2)
+        task.f1_scorer(binary_scores, binary_targets)
+
+        if self.loss_type == 'softmax':
+            # softmax over each row.
+            return F.cross_entropy(logits, flat_labels)
+        elif self.loss_type == 'sigmoid':
+            return F.binary_cross_entropy(F.sigmoid(logits),
+                                          binary_targets.float())
+        else:
+            raise ValueError("Unsupported loss type '%s' "
+                             "for edge probing." % loss_type)
