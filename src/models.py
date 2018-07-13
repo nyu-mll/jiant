c5d38414cb22733303b29da2a0aff3f35f40ae53
Jan Hůla <jan.hula@osu.cz>
2018-07-12 20:51:16 -0400

    fix for ( val_interval = 1, val_data_limit = 33) and for  ( dataset_size % batch_size == 1 )

--- src/models.py
+++ src/models.py
@@ -485,7 +485,9 @@ class MultiTaskModel(nn.Module):
         out['n_exs'] = get_batch_size(batch)
 
         if 'labels' in batch: # means we should compute loss
-            if batch['labels'].dim() <= 1:
+            if batch['labels'].dim() == 0:
+                labels = batch['labels'].unsqueeze(0)
+            elif batch['labels'].dim() == 1:
                 labels = batch['labels']
             else:
                 labels = batch['labels'].squeeze(-1)
