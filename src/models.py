fix alignment between decoder input vs target very carefully to be off by one. inputs should be :-1 and targets should be 1:

src/models.py
@@ -647,13 +647,15 @@ class MultiTaskModel(nn.Module):
             decoder = getattr(self, "%s_decoder" % task.name)
             out.update(decoder.forward(sent, sent_mask, batch['targs']))
             task.scorer1(math.exp(out['loss'].item()))
-            return out
 
             if True:  # if predict:
                 # bleu scoring
+                import pdb; pdb.set_trace()
                 bleu_score = beamsearch.generate_and_compute_bleu(decoder, sent, sent_mask, batch['targs'])
                 task.scorer2(bleu_score)
                 print(bleu_score)
+
+            return out
 
         if 'targs' in batch:
             pass