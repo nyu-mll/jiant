525f8ec0d3a252e747f13581be54631c12b6ea16
yukatherin <yu.katherin@gmail.com>
2018-07-18 16:28:12 -0400

    add warnings for preliminary bleu scoring, only import where used

--- src/bleu_scoring.py
+++ src/bleu_scoring.py
@@ -6,6 +6,8 @@ import math
 import numpy as np
 import sys
 
+print("Warning -- this is a preliminary version of BLEU scoring, it has not been verified!")
+
 
 def bleu_stats(hypothesis, reference):
     """Compute statistics for BLEU."""
