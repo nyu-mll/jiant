MT: fix target endtoken; masking encoder context; add beam search (#184)
* [MT] add beam search on decoder

* fix start/end token; removing dropout in decoder temporarily

* [MT] sos/eos fixed

* dont commit data path change

* fix alignment between decoder input vs target very carefully to be off by one. inputs should be :-1 and targets should be 1:

* fix alignment between decoder input vs target very carefully to be off by one, after putting START_SYMBOL,END_SYMBOL in targs. inputs should be :-1 and targets should be 1:

* fix alignment between decoder input vs target very carefully to be off by one. inputs should be :-1 and targets should be 1:

* beam search is now running, need to add bleu scoring (and possibly debug)

* remove pdb, and restore paths

* dont checkin "if True:"

* PR175 deal with comments and rm example_train_for_eval_run.sh

* a lot of (careful) cleanups; refactor initialize hidden/context states; make sure attention uses 0-masking and maxpooling uses -inf-masking

* fixes for beam search with attention (need to repeat encoder output), incl update_active

* add mt_attention args \n test: python main.py --config config/defaults.conf --overrides "exp_name = test-wmt, train_tasks = wmt14_en_de, eval_tasks = wmt14_en_de, run_name = wmt_elmo_no_attention_noelmo, elmo = 1, elmo_chars_only = 1, dropout = 0.2, lr = 0.001, val_interval = 10000, max_vals = 50, batch_size = 20, max_word_v_size = 20000, max_seq_len=20, mt_attention=none

* leave this under if False for now, will update when bleu scoring is ready

* make args.mt_attention optional

* minor warning comment in beam search

* clean up debug options in seq2seq_decoder

* beamsearch - code simplification

* 184 - address ian's comments

* integrate the python (unvalidated) version of bleu scoring, which looks reasonably ok so far

* infrastructure for a real run with bleu

* minor - remove unused in seq2seq_decoder

* encoder_outputs.clone() is unne

src/models.py
@@ -29,6 +29,7 @@ from .allennlp_mods.elmo_text_field_embedder import ElmoTextFieldEmbedder, ElmoT
 from .utils import get_batch_utilization, get_elmo_mixing_weights
 from . import config
 from . import edge_probing
+from . import beamsearch
 
 from .tasks import STSBTask, CoLATask, SSTTask, \
     PairClassificationTask, SingleClassificationTask, \
@@ -277,11 +278,13 @@ def build_module(task, model, d_sent, d_emb, vocab, embedder, args):
         module = edge_probing.EdgeClassifierModule(task, d_sent, task_params)
         setattr(model, '%s_mdl' % task.name, module)
     elif isinstance(task, (MTTask, Reddit_MTTask)):
+        attention = args.get("mt_attention", "bilinear")
+        log.info("using {} attention".format(attention))
         decoder_params = Params({'input_dim': d_sent,
                                  'target_embedding_dim': 300,
                                  'max_decoding_steps': 200,
-                                 'target_namespace': 'tokens',
-                                 'attention': 'bilinear',
+                                 'target_namespace': 'targets',
+                                 'attention': attention,
                                  'dropout': args.dropout,
                                  'scheduled_sampling_ratio': 0.0})
         decoder = Seq2SeqDecoder.from_params(vocab, decoder_params)
@@ -652,6 +655,12 @@ class MultiTaskModel(nn.Module):
             decoder = getattr(self, "%s_decoder" % task.name)
             out.update(decoder.forward(sent, sent_mask, batch['targs']))
             task.scorer1(math.exp(out['loss'].item()))
+
+            if not self.training:
+                # bleu scoring
+                bleu_score = beamsearch.generate_and_compute_bleu(decoder, sent, sent_mask, batch['targs'])
+                task.scorer2(bleu_score)
+
             return out
 
         if 'targs' in batch:
@@ -789,4 +798,3 @@ class MultiTaskModel(nn.Module):
                 else:
                     params["@pretrain@"] = get_elmo_mixing_weights(self.sent_encoder._text_field_embedder, task=None)
         return params
-