77c44817e5d42d6e1963ff795650ec14558d186f
Ian Tenney <iftenney@gmail.com>
2018-07-22 07:49:37 +0000

    Separate exp due to separate vocabulary

--- major_experiment_scripts/edgeprobe_elmo_baselines.sh
+++ major_experiment_scripts/edgeprobe_elmo_baselines.sh
@@ -5,7 +5,7 @@
 # we do this as a series of sequential runs, with one task per run.
 
 function run_exp() {
-    OVERRIDES="exp_name=edgeprobe-bare-chars2, run_name=$1"
+    OVERRIDES="exp_name=edgeprobe-bare-chars-$1, run_name=$1"
     OVERRIDES+=", train_tasks=$1"
     python main.py --config_file config/edgeprobe_bare.conf \
         -o "${OVERRIDES}" \
@@ -17,7 +17,7 @@ set -eux
 cd $(dirname $0)
 pushd "${PWD%jiant*}/jiant"
 
-run_exp "edges-srl-conll2005"
+# run_exp "edges-srl-conll2005"
 run_exp "edges-spr2"
 run_exp "edges-dpr"
 run_exp "edges-coref-ontonotes"
