88da1bdd4a9fe11967adf7cb8d0344f8f09743e8
Ian Tenney <iftenney@gmail.com>
2018-07-25 22:09:51 +0000

    Notify email option

--- major_experiment_scripts/edgeprobe_full_elmo_baselines.sh
+++ major_experiment_scripts/edgeprobe_full_elmo_baselines.sh
@@ -3,12 +3,14 @@
 # Script to run edge probing on ELMo-only.
 # Run as separate experiments, since datasets are disjoint anyway.
 
+NOTIFY_EMAIL=$1
+
 function run_exp() {
     OVERRIDES="exp_name=elmo-full-$1, run_name=run"
     OVERRIDES+=", eval_tasks=$1, elmo_chars_only=0"
     python main.py --config_file config/edgeprobe_bare.conf \
         -o "${OVERRIDES}" \
-        --remote_log --notify iftenney@gmail.com
+        --remote_log --notify "$NOTIFY_EMAIL"
 }
 
 set -eux
