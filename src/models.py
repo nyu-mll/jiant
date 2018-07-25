6d4b92c6270fddd89c2b571f54cdbc00c9471480
hyinghui <huangyinghui14@gmail.com>
2018-07-25 16:10:35 +0000

    skip bleu in valid step for Wiki103_MTTask

--- src/models.py
+++ src/models.py
@@ -724,7 +724,7 @@ class MultiTaskModel(nn.Module):
             out.update(decoder.forward(sent, sent_mask, batch['targs']))
             task.scorer1(math.exp(out['loss'].item()))
 
-            if not self.training:
+            if not self.training and not isinstance(task, Wiki103_MTTask):
                 # bleu scoring
                 bleu_score = beamsearch.generate_and_compute_bleu(decoder, sent, sent_mask, batch['targs']['words'], preds_file_path=task.preds_file_path)
                 task.scorer2(bleu_score)
