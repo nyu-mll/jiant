b009f399e6617956b09bfa89721e1405e0f7826e
Sam Bowman <sbowman@stanford.edu>
2018-07-25 13:00:53 -0400

    Refactor.

--- major_experiment_scripts/extract_diagnostic_set_results.py
+++ major_experiment_scripts/extract_diagnostic_set_results.py
@@ -7,39 +7,47 @@ if len(sys.argv) < 2:
     print("Usage: python extract_results.py results.tsv")
     exit(0)
 
+def get_strings(path, row_filter=None):
+    strings = []
+    with open(path) as f:
+      for line in f:
+        if not "mnli-diagnostic" in line:
+            continue
+        if row_filter and not row_filter in line:
+            continue
+
+        name, results = line.strip().split(None, 1)
+
+        coarse = {}
+        fine = defaultdict(dict)
+        outstr = name + "\t"
+
+        for result in results.split(', '):
+            dataset, value = result.split(': ')
+            value = float(value) * 100
+            if 'accuracy' in dataset or 'mnli-diagnostic' not in dataset or dataset == "":
+                continue
+            subset = dataset.split('mnli-diagnostic_', 1)[1]
+            sp = subset.split('__')
+            if len(sp) == 1 or sp[1] == 'missing':
+                coarse[sp[0]] = value
+            else:
+                fine[sp[0]][sp[1]] = value
+
+        for key in sorted(coarse.keys()):
+            outstr += "%.02f\t" % coarse[key]
+
+        for key in sorted(fine.keys()):
+            for inner_key in sorted(fine[key].keys()):
+                outstr += "%.02f\t" % fine[key][inner_key]
+        strings.append(outstr)
+    return strings
+
 for path in sys.argv[1:]:
     try:
         print(path)
-        with open(path) as f:
-          for line in f:
-            if not "mnli-diagnostic" in line:
-                continue
-
-            name, results = line.strip().split(None, 1)
-
-            coarse = {}
-            fine = defaultdict(dict)
-            outstr = name + "\t"
-
-            for result in results.split(', '):
-                dataset, value = result.split(': ')
-                value = float(value) * 100
-                if 'accuracy' in dataset or 'mnli-diagnostic' not in dataset or dataset == "":
-                    continue
-                subset = dataset.split('mnli-diagnostic_', 1)[1]
-                sp = subset.split('__')
-                if len(sp) == 1 or sp[1] == 'missing':
-                    coarse[sp[0]] = value
-                else:
-                    fine[sp[0]][sp[1]] = value
-
-            for key in sorted(coarse.keys()):
-                outstr += "%.02f\t" % coarse[key]
-
-            for key in sorted(fine.keys()):
-                for inner_key in sorted(fine[key].keys()):
-                    outstr += "%.02f\t" % fine[key][inner_key]
-
+        strings = get_strings(path)
+        for outstr in strings:
             print(outstr)
 
     except BaseException as e:
