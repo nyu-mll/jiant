9bf4a16a45b989bde6835c95777f254f747b5e27
Roma Patel <romapatel996@gmail.com>
2018-07-25 16:58:48 -0400

    Update final_glue_runs.sh

--- major_experiment_scripts/final_glue_runs.sh
+++ major_experiment_scripts/final_glue_runs.sh
@@ -80,8 +80,7 @@ JIANT_OVERRIDES="train_tasks = reddit, run_name = reddit-elmo, elmo_chars_only =
 
 ## MSCOCO ##
 
-# TODO: Roma - Make sure this uses the dataset and objective function you want.
-# TODO: Roma - Add a version that is just shapeworld, too (w/ and w/o elmo)
+# TODO: Move images/features to one fixed directory
 
 JIANT_OVERRIDES="train_tasks = grounded, run_name = grounded-noelmo, elmo_chars_only = 1" JIANT_CONF="config/final.conf" sbatch nyu_cilvr_cluster.sbatch
 JIANT_OVERRIDES="train_tasks = grounded, run_name = grounded-elmo, elmo_chars_only = 0, sep_embs_for_skip = 1" JIANT_CONF="config/final.conf" sbatch nyu_cilvr_cluster.sbatch
