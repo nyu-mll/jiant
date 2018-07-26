1fefd71cd95534089b9c0fbcb3c6f9dd40eee44b
Alex Wang <wang.alex.c@gmail.com>
2018-07-26 17:58:35 -0400

    Revert "[MT] greedy decoding (#251)" (#258)
    This reverts commit 852d7bd6143faa1acdd4ef47a2fe84372f3b48c9.

--- src/models.py
+++ src/models.py
@@ -729,7 +729,7 @@ class MultiTaskModel(nn.Module):
             if not self.training and not isinstance(task, Wiki103_Seq2Seq):
                 # bleu scoring
                 bleu_score, unk_ratio_macroavg = beamsearch.generate_and_compute_bleu(decoder, sent, sent_mask, batch['targs']['words'], preds_file_path=task.preds_file_path, task=task)
-                # task.scorer2(bleu_score)
+                task.scorer2(bleu_score)
                 task.scorer3(unk_ratio_macroavg)
 
             return out
