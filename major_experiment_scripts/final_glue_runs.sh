8feb9618a07a099ad65439c4c78a9ff7ca061dfd
Sam Bowman <sbowman@stanford.edu>
2018-07-27 18:03:54 -0400

    Claim change.
    Sneaking around the PR process.

--- major_experiment_scripts/final_glue_runs.sh
+++ major_experiment_scripts/final_glue_runs.sh
@@ -129,7 +129,7 @@ JIANT_OVERRIDES="train_tasks = grounded, run_name = grounded-elmo, elmo_chars_on
 
 ## CCG (Note: For use in the NLI probing paper only) ##
 
-# Sam is running.
+# Pending fix from Jan (or Tom).
 JIANT_OVERRIDES="train_tasks = ccg, run_name = ccg-noelmo, elmo_chars_only = 1" JIANT_CONF="config/final.conf" sbatch nyu_cilvr_cluster.sbatch
 JIANT_OVERRIDES="train_tasks = ccg, run_name = ccg-elmo, elmo_chars_only = 0, sep_embs_for_skip = 1" JIANT_CONF="config/final.conf" sbatch nyu_cilvr_cluster.sbatch
 
