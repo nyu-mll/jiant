1f3df15c9058622825dce711ffbd823d7e3e2589
yukatherin <yu.katherin@gmail.com>
2018-07-28 22:29:37 -0400

    add comment

--- src/preprocess.py
+++ src/preprocess.py
@@ -480,7 +480,7 @@ def get_words(tasks):
 
     for task in tasks:
         log.info("\tCounting words for task: '%s'", task.name)
-        if isinstance(task, MTEnRuTask):
+        if isinstance(task, MTEnRuTask):  # TODO: change tasks so this is MTTask
             for src_sent, tgt_sent in task.get_sentences():
                 update_vocab_freqs(src_sent)
                 update_target_vocab_freqs(tgt_sent)
