e9470831fc0070b8693ac7c58b7d7cafa30bdbe7
najoung <sa_112949933820817028848@najoung-k15.c.jsalt-sentence-rep.internal>
2018-07-31 16:37:00 +0000

    added negation/prepswap commands

--- major_experiment_scripts/final_nliprobing_runs.sh
+++ major_experiment_scripts/final_nliprobing_runs.sh
@@ -2,7 +2,6 @@
 
 MODEL_DIR=$1 # directory of checkpoint to probe, e.g: /nfs/jsalt/share/models_to_probe/nli_do2_noelmo
 PROBING_TASK=$2 # task name, e.g. recast-puns
-
 EXP_NAME="probing"
 PARAM_FILE=${MODEL_DIR}"/params.conf"
 MODEL_FILE=${MODEL_DIR}"/model_state_eval_best.th"
@@ -10,22 +9,22 @@ MODEL_FILE=${MODEL_DIR}"/model_state_eval_best.th"
 # Note: you should only be overriding run_name and eval_tasks and (maybe) something like probe_path. 
 
 # Example of run that would probe on puns
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = recast-puns, eval_tasks = recast-puns, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = recast-puns, eval_tasks = recast-puns, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
 
 # TODO(Najoung) Prepositions, Negations
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = prepswap, eval_tasks = nli-prob-prepswap, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, nli-prob-prepswap_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = negation, eval_tasks = nli-prob-negation, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, nli-prob-negation_use_classifier=mnli"
 
 # TODO(Tom) NPs 
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
 
 # TODO(Roma) Spatial, Quantifiers, Appearence, Comparators
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
 
 # TODO(Alexis) Implicatives, Factives, Neutrals
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
-python main.py -c config/defaults.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
+python main.py -c config/final.conf ${PARAM_FILE} config/eval_existing.conf -o "run_name = ???, eval_tasks = ???, load_eval_checkpoint = ${MODEL_FILE}, exp_name = ${EXP_NAME}, ${PROBING_TASK}_use_classifier=mnli"
