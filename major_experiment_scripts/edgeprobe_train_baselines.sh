0aea04d095e87524d2d6ee57afe6687dfeb7f27d
Ian Tenney <iftenney@gmail.com>
2018-08-01 03:33:42 +0000

    Update tasks & scripts. Consistent intervals for OntoNotes.

--- major_experiment_scripts/edgeprobe_train_baselines.sh
+++ major_experiment_scripts/edgeprobe_train_baselines.sh
@@ -6,7 +6,7 @@
 NOTIFY_EMAIL=$1
 
 function run_exp() {
-    OVERRIDES="exp_name=train-$1, run_name=run"
+    OVERRIDES="exp_name=train-chars-$1, run_name=run"
     OVERRIDES+=", train_tasks=$1, max_vals=$2, val_interval=$3"
     python main.py --config_file config/edgeprobe_train.conf \
         -o "${OVERRIDES}" \
@@ -18,11 +18,19 @@ set -eux
 cd $(dirname $0)
 pushd "${PWD%jiant*}/jiant"
 
-run_exp "edges-srl-conll2005" 200 500
+# Small tasks
 run_exp "edges-spr2" 100 100
 run_exp "edges-dpr" 100 100
-run_exp "edges-coref-ontonotes" 200 500
 run_exp "edges-dep-labeling" 200 500
-
 run_exp "edges-ner-conll2003" 200 250
-run_exp "edges-constituent-ptb" 200 500
+
+# OntoNotes
+run_exp "edges-srl-conll2012" 200 1000
+run_exp "edges-coref-ontonotes-conll" 200 1000
+run_exp "edges-ner-ontonotes" 200 1000
+run_exp "edges-constituent-ontonotes" 200 1000
+
+# run_exp "edges-srl-conll2005"
+# run_exp "edges-coref-ontonotes"
+# run_exp "edges-constituent-ptb"
+# run_exp "edges-ccg-tag"
--- major_experiment_scripts/edgeprobe_train_full_baselines.sh
+++ major_experiment_scripts/edgeprobe_train_full_baselines.sh
@@ -7,8 +7,7 @@ NOTIFY_EMAIL=$1
 
 function run_exp() {
     OVERRIDES="exp_name=train-full-$1, run_name=run"
-    OVERRIDES+=", elmo_chars_only=0"
-    OVERRIDES+=", train_tasks=$1, max_vals=$2, val_interval=$3"
+    OVERRIDES+=", train_tasks=$1, max_vals=$2, val_interval=$3, elmo_chars_only=0"
     python main.py --config_file config/edgeprobe_train.conf \
         -o "${OVERRIDES}" \
         --remote_log --notify "$NOTIFY_EMAIL"
@@ -19,11 +18,19 @@ set -eux
 cd $(dirname $0)
 pushd "${PWD%jiant*}/jiant"
 
-run_exp "edges-srl-conll2005" 200 500
+# Small tasks
 run_exp "edges-spr2" 100 100
 run_exp "edges-dpr" 100 100
-run_exp "edges-coref-ontonotes" 200 500
 run_exp "edges-dep-labeling" 200 500
-
 run_exp "edges-ner-conll2003" 200 250
-run_exp "edges-constituent-ptb" 200 500
+
+# OntoNotes
+run_exp "edges-srl-conll2012" 200 1000
+run_exp "edges-coref-ontonotes-conll" 200 1000
+run_exp "edges-ner-ontonotes" 200 1000
+run_exp "edges-constituent-ontonotes" 200 1000
+
+# run_exp "edges-srl-conll2005"
+# run_exp "edges-coref-ontonotes"
+# run_exp "edges-constituent-ptb"
+# run_exp "edges-ccg-tag"
