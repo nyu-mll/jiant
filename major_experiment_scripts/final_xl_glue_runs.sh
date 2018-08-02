fe20ae31965d16e06005489d39b85de2db78d000
w4ngatang <wang.alex.c@gmail.com>
2018-08-01 21:06:25 -0400

    Fix batch size; update comments in final_xl conf and script

--- major_experiment_scripts/final_xl_glue_runs.sh
+++ major_experiment_scripts/final_xl_glue_runs.sh
@@ -1,22 +1,19 @@
-# These are the pretraining runs that will be included in our three main papers.
+# These pretraining runs explore pretraining a larger encoder than in the main experiments (final_glue_runs.sh).
+# We privilege three subsets of the pretraining tasks: LM, MTL on GLUE, MTL on non-GLUE
 
 # Ground rules:
 # - There will be a separate tab in the sheet for these results. All results in the paper will come from that tab,
 #     and all results in that tab must be from the runs described here.
 # - All runs must be from the master branch.
-# - We will not change defaults.conf, to preserve compatibility with older runs. Shared defaults will go in final.conf.
+# - We will not change defaults.conf, to preserve compatibility with older runs.
+#     - Defaults shared with the main (non-XL) experiments will go in final.conf.
+#     - Defaults between XL experiments will go in final_xl.conf.
 # - You may not modify the overrides in these commands except through a reviewed pull request.
-# - No pull requests will be allowed after 5p on Tuesday 7/24, except in cases of _mistakes_ in the commands below.
+# - No pull requests will be allowed, except in cases of _mistakes_ in the commands below.
 # - All results on the sheet must be reported through the script. You may not manually type anything in the main area of the sheet.
 # - These commands are set up for NYU. You may, of course, modify everything outside the quotation marks to suit your machine setup.
 #     Make sure you use final.conf if you do.
-# - All runs will be started by 5p on Friday 7/27.
 
-
-### Random BiLSTM, no pretraining ###
-
-JIANT_OVERRIDES="train_tasks = none, allow_untrained_encoder_parameters = 1, do_train = 0, run_name = random-noelmo-xl, elmo_chars_only = 1" JIANT_CONF="config/final_xl.conf" sbatch nyu_cilvr_cluster.sbatch
-JIANT_OVERRIDES="train_tasks = none, allow_untrained_encoder_parameters = 1, do_train = 0, run_name = random-elmo-xl, elmo_chars_only = 0, sep_embs_for_skip = 1" JIANT_CONF="config/final_xl.conf" sbatch nyu_cilvr_cluster.sbatch
 
 ### LM ###
 
