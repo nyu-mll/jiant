b8ea82c7602fb93b617778fa212ee10be4ff230f
pitrack <paxia@jhu.edu>
2018-08-27 23:09:31 -0400

    fix merge issue

--- major_experiment_scripts/edgeprobe_elmo_baselines.sh
+++ major_experiment_scripts/edgeprobe_elmo_baselines.sh
@@ -3,21 +3,14 @@
 # Script to run edge probing on ELMo char-CNN only.
 # Run as separate experiments, since datasets are disjoint anyway.
 
-<<<<<<< HEAD
-=======
 NOTIFY_EMAIL=$1
 
->>>>>>> master
 function run_exp() {
     OVERRIDES="exp_name=elmo-chars-$1, run_name=run"
     OVERRIDES+=", eval_tasks=$1"
     python main.py --config_file config/edgeprobe_bare.conf \
         -o "${OVERRIDES}" \
-<<<<<<< HEAD
-        --remote_log --notify iftenney@gmail.com
-=======
-        --remote_log --notify "$NOTIFY_EMAIL"
->>>>>>> master
+        # --remote_log --notify "$NOTIFY_EMAIL"
 }
 
 set -eux
@@ -25,12 +18,12 @@ set -eux
 cd $(dirname $0)
 pushd "${PWD%jiant*}/jiant"
 
-run_exp "edges-srl-conll2005"
-run_exp "edges-spr2"
-run_exp "edges-dpr"
-run_exp "edges-coref-ontonotes"
-run_exp "edges-dep-labeling"
+#run_exp "edges-srl-conll2005"
+#run_exp "edges-spr2"
+#run_exp "edges-dpr"
+#run_exp "edges-coref-ontonotes"
+#run_exp "edges-dep-labeling"
 
 run_exp "edges-ner-conll2003"
-run_exp "edges-constituent-ptb"
+#run_exp "edges-constituent-ptb"
 # run_exp "edges-ccg-tag"
