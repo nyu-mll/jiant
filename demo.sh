1db17b9ca656601218afbee58f2c4b977e02123f
w4ngatang <wang.alex.c@gmail.com>
2018-09-04 09:50:44 -0400

    Remove having statefulness as an option

--- demo.sh
+++ demo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+source user_config.sh
+
 # Quick-start: run this
-python main.py --config_file config/demo.conf
+python main.py --config_file config/demo.conf --overrides "run_name = mtl-sst-mrpc-v2"
