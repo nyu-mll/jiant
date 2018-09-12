1272e800c150cdabec1422c049e3182cd42c7676
Ian Tenney <iftenney@users.noreply.github.com>
2018-09-12 14:42:42 -0700

    Update edge data script (#408)

--- probing/edge_data_stats.py
+++ probing/edge_data_stats.py
@@ -3,14 +3,16 @@
 # Helper script to get stats on edge-probing data.
 #
 # Usage:
-#  python edge_data_stats.py /path/to/edge/probing/data/*.json
+#  python edge_data_stats.py -i /path/to/edge/probing/data/*.json -o stats.tsv
 #
-# Will print dataset size, num targets, etc. to stdout.
+# Will print dataset size, num targets, etc. to stdout, and optionally write
+# stats to a TSV file if -o <file> is given.
 
 import sys
 import os
 import json
 import collections
+import argparse
 from tqdm import tqdm
 
 import logging as log
@@ -26,11 +28,25 @@ def analyze_file(fname: str):
     record_iter = utils.load_json_data(fname)
     stats = utils.EdgeProbingDatasetStats()
     stats.compute(record_iter)
-    log.info(stats.format())
+    log.info(stats.format(_name=fname))
+    return stats.to_series(_name=fname)
 
 def main(args):
-    for fname in args:
-        analyze_file(fname)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', dest='output', type=str, default="",
+                        help="Output file (TSV).")
+    parser.add_argument('-i', dest='inputs', type=str, nargs="+",
+                        help="Input files.")
+    args = parser.parse_args(args)
+
+    all_stats = []
+    for fname in args.inputs:
+        all_stats.append(analyze_file(fname))
+    df = pd.DataFrame(all_stats)
+    df.set_index("_name", inplace=True)
+    if args.output:
+        log.info("Writing stats table to %s", args.output)
+        df.to_csv(args.output, sep="\t")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
