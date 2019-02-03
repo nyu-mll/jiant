113b0ab1dfa8096702c2ce194d512f65f9e0b717
pruksmhc <pruksmhc@mtholyoke.edu>
2019-02-03 13:07:10 -0500

    adding unit tests

---
+++ tests/util_test.py
@@ -0,0 +1,16 @@
+import ..src.utils.utils as utils
+import unittest
+from io import StringIO
+
+def test_load_tsv_has_labels_one_sentence():
+    mock_csv = StringIO("sentence	label",
+    "it 's a charming and often affecting journey . 	1",
+    "unflinchingly bleak and desperate 	0")
+    with open('temp_output.tsv', 'w', newline='') as f_output:
+        tsv_output = csv.writer(f_output, delimiter='\t')
+        tsv_output.writerow(data)
+    max_seq_len = 30
+    data = load_tsv('data_output.tsv'), max_seq_len,
+                       s1_idx=0, s2_idx=None, label_idx=1, skip_rows=1)
+    # assert lenght of s1 is more than 1, length of s2 is nothing,
+    # label_idx is 1, and then theheaders is not there.
