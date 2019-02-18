177c8de1b860d4ac0be0f9f73939f87e4ddb7bee
Ian Tenney <iftenney@gmail.com>
2019-02-18 23:49:31 +0000

    Check out script

---
+++ scripts/edges/analyze_project.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Convenience script to analyze a directory ("project") of experiments.
+#
+# Usage: ./analyze_project.sh /path/to/project
+
+PROJECT_DIR=$1
+
+set -eu
+
+if [ -z $PROJECT_DIR ]; then
+    echo "You must provide a project directory!"
+    exit 1
+fi
+
+THIS_DIR=$(realpath $(dirname $0))
+JIANT_DIR=${THIS_DIR%jiant*}/jiant
+
+all_runs=( ${PROJECT_DIR}/*/run* )
+echo "Found runs:"
+for run in "${all_runs[@]}"; do
+  echo $run
+done
+
+nproc=$( grep -c ^processor /proc/cpuinfo )
+nparallel=$( expr $nproc - 1 )
+
+set -x
+
+python $JIANT_DIR/probing/analyze_runs.py \
+    -i "${all_runs[@]}" -o ${PROJECT_DIR}/scores.tsv \
+    --parallel $(( $nparallel > 10 ? 10 : $nparallel ))
+
