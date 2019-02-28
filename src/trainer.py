c49c5755c76e0bec041879d842a78cd5bfbdf1c1
Yada Pruksachatkun <yp913@cassio.cs.nyu.edu>
2019-02-28 18:19:47 -0500

    changed condition to optimizer_params

--- src/trainer.py
+++ src/trainer.py
@@ -318,7 +318,7 @@ class SamplingMultiTaskTrainer:
         if shared_optimizer:  # if shared_optimizer, ignore task_specific optimizers
             if self._max_epochs_per_task:
                 n_epoch_steps = sum([info["n_tr_batches"] for info in task_infos.values()])
-                if self._model.use_bert:
+                if optimizer_params["type"] == "bert_adam":
                     optimizer_params["t_total"] = n_epoch_steps * self._max_epochs_per_task
             g_optimizer = Optimizer.from_params(train_params, copy.deepcopy(optimizer_params))
             g_scheduler = LearningRateScheduler.from_params(
