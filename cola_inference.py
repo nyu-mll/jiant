a646c5c72ad6bf4a742b31912605a1ef3916dec4
Jason Phang <email@jasonphang.com>
2019-03-09 19:21:46 -0600

    Added evaluation for inference

--- cola_inference.py
+++ cola_inference.py
@@ -13,8 +13,18 @@ To process a corpus:
     python cola_inference.py \
         --config_file PATH_TO_CONFIG_FILE \
         --model_file_path PATH_TO_FILE_PATH \
-        --inference mode corpus \
-        --input_path PATH_TO_INPUT_CORPUS
+        --inference_mode corpus \
+        --input_path PATH_TO_INPUT_CORPUS \
+        --output_path PATH_TO_WRITE_OUTPUT
+
+To process+evaluate (e.g.) the CoLA dev set:
+
+    python cola_inference.py \
+        --config_file PATH_TO_CONFIG_FILE \
+        --model_file_path PATH_TO_FILE_PATH \
+        --inference_mode corpus \
+        --input_path PATH_TO_INPUT_CORPUS \
+        --input_format dev \
         --output_path PATH_TO_WRITE_OUTPUT
 
 (Ensure that the repository is in your PYTHONPATH when running this script.)
@@ -22,6 +32,7 @@ To process a corpus:
 '''
 # pylint: disable=no-member
 import argparse
+import json
 import numpy as np
 import os
 import pandas as pd
@@ -35,6 +46,7 @@ import torch
 from src.tasks.tasks import process_sentence, sentence_to_text_field
 from src.utils import config
 from src.utils.utils import load_model_state, check_arg_name
+from src.utils.data_loaders import load_tsv
 from src.preprocess import build_tasks, build_indexers
 from src.models import build_model
 
@@ -66,8 +78,13 @@ def handle_arguments(cl_arguments):
                         help="Input path for running inference."
                              " One input per line."
                              " Only in eval_mode='corpus'")
+    parser.add_argument('--input_format', type=str, default="text",
+                        help="Format of input (text | train | dev | test)")
     parser.add_argument('--output_path', type=str, default=None,
                         help="Output path for running inference."
+                             " Only in eval_mode='corpus'")
+    parser.add_argument('--eval_output_path', type=str, default=None,
+                        help="Output path for metrics from evaluation."
                              " Only in eval_mode='corpus'")
 
     return parser.parse_args(cl_arguments)
@@ -121,7 +138,11 @@ def main(cl_arguments):
         run_repl(model, vocab, indexers, task, args)
     elif cl_args.inference_mode == "corpus":
         run_corpus_inference(model, vocab, indexers, task, args,
-                             cl_args.input_path, cl_args.output_path)
+                             cl_args.input_path,
+                             cl_args.input_format,
+                             cl_args.output_path,
+                             cl_args.eval_output_path,
+                             )
     else:
         raise KeyError(cl_args.inference_mode)
 
@@ -164,13 +185,13 @@ def run_repl(model, vocab, indexers, task, args):
 
 
 def run_corpus_inference(model, vocab, indexers, task, args,
-                         input_path, output_path):
+                         input_path, input_format, output_path,
+                         eval_output_path):
     ''' Run inference on corpus '''
-    with open(input_path, "r") as f_in:
-        data_in = f_in.readlines()
+    tokens, labels = load_cola_data(input_path, task, input_format, args.max_seq_len)
     logit_batches = []
-    for batch_string_ls in tqdm(list(batchify(data_in, args.batch_size))):
-        batch, _ = prepare_batch(batch_string_ls, task, vocab, indexers, args)
+    for tokens_batch in tqdm(list(batchify(tokens, args.batch_size))):
+        batch, _ = prepare_batch(tokens_batch, vocab, indexers, args)
         with torch.no_grad():
             out = model.forward(task, batch, predict=True)
             logit_batches.append(out["logits"].cpu().numpy())
@@ -194,6 +215,16 @@ def run_corpus_inference(model, vocab, indexers, task, args,
 
     df.to_csv(output_path, index=False)
 
+    if labels is not None:
+        metrics = get_cola_metrics(logits, preds, labels, task)
+        metrics_str = json.dumps(metrics, indent=2)
+        print(metrics_str)
+        if eval_output_path is not None:
+            with open(eval_output_path, "w") as f:
+                f.write(metrics_str)
+    else:
+        assert eval_output_path is None
+
 
 def batchify(ls, batch_size):
     ''' Partition a list into batches of batch_size '''
@@ -203,16 +234,11 @@ def batchify(ls, batch_size):
         i += batch_size
 
 
-def prepare_batch(input_string_ls, task, vocab, indexers, args):
+def prepare_batch(tokens_batch, vocab, indexers, args):
     ''' Do preprocessing for batch '''
     instance_ls = []
     token_ls = []
-    for input_string in input_string_ls:
-        tokens = process_sentence(
-            tokenizer_name=task.tokenizer_name,
-            sent=input_string,
-            max_seq_len=args.max_seq_len,
-        )
+    for tokens in tokens_batch:
         field = sentence_to_text_field(tokens, indexers)
         field.index(vocab)
         instance_ls.append(Instance({"input1": field}))
@@ -226,6 +252,41 @@ def take_one(ls):
     ''' Extract singleton from list '''
     assert len(ls) == 1
     return ls[0]
+
+
+def load_cola_data(input_path, task, input_format, max_seq_len):
+    if input_format == "text":
+        with open(input_path, "r") as f_in:
+            sentences = f_in.readlines()
+        tokens = [
+            process_sentence(
+                tokenizer_name=task.tokenizer_name,
+                sent=sentence,
+                max_seq_len=max_seq_len,
+            )
+            for sentence in sentences
+        ]
+        labels = None
+    elif input_format == "train" or input_format == "dev":
+        data = load_tsv(task.tokenizer_name, input_path, max_seq_len,
+                        s1_idx=3, s2_idx=None, label_idx=1)
+        tokens, labels = data[0], data[2]
+    elif input_format == "test":
+        data = load_tsv(task.tokenizer_name, input_path, max_seq_len,
+                        s1_idx=1, s2_idx=None, has_labels=False, return_indices=True, skip_rows=1)
+        tokens, labels = data[0], None
+    else:
+        raise KeyError(input_format)
+    return tokens, labels
+
+
+def get_cola_metrics(logits, preds, labels, task):
+    labels_tensor = torch.tensor(np.array(labels))
+    logits_tensor = torch.tensor(logits)
+    preds_tensor = torch.tensor(preds)
+    task.scorer2(logits_tensor, labels_tensor)
+    task.scorer1(labels_tensor, preds_tensor)
+    return task.get_metrics(reset=True)
 
 
 if __name__ == '__main__':
