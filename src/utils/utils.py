5a2311988e0ba33f65a05168661f8bdd7e2147f3
w4ngatang <wang.alex.c@gmail.com>
2019-03-13 12:54:54 -0400

    Add back MosesDetokenizer

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,6 +17,8 @@ from torch.nn import Dropout, Linear
 from torch.nn import Parameter
 from torch.nn import init
 
+from nltk.tokenize.moses import MosesDetokenizer
+
 from allennlp.nn.util import masked_softmax, device_mapping
 from allennlp.common.checks import ConfigurationError
 from allennlp.modules.seq2seq_encoders.seq2seq_encoder import Seq2SeqEncoder
