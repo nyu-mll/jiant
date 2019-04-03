7a1d9b9ea0ad50e5fe348592d88992d6118fc4b2
w4ngatang <wang.alex.c@gmail.com>
2019-04-03 12:55:34 -0400

    Fix merge problem; print test results (tmp)

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def evaluate_and_write(args, model, tasks, splits_to_write):
         evaluate.write_preds(tasks, val_preds, args.run_dir, 'val',
                              strict_glue_format=args.write_strict_glue_format)
     if 'test' in splits_to_write:
-        _, te_preds = evaluate.evaluate(model, tasks, args.batch_size, args.cuda, "test")
+        te_results, te_preds = evaluate.evaluate(model, tasks, args.batch_size, args.cuda, "test")
         evaluate.write_preds(tasks, te_preds, args.run_dir, 'test',
                              strict_glue_format=args.write_strict_glue_format)
     run_name = args.get("run_name", os.path.basename(args.run_dir))
@@ -125,6 +125,8 @@ def evaluate_and_write(args, model, tasks, splits_to_write):
     results_tsv = os.path.join(args.exp_dir, "results.tsv")
     log.info("Writing results for split 'val' to %s", results_tsv)
     evaluate.write_results(val_results, results_tsv, run_name=run_name)
+    if 'test' in splits_to_write:
+        evaluate.write_results(te_results, results_tsv, run_name=run_name)
 
 
 def main(cl_arguments):
--- src/bert/utils.py
+++ src/bert/utils.py
@@ -35,30 +35,6 @@ def _get_seg_ids(ids, sep_id):
     return seg_ids
 
 
-def _get_seg_ids(ids, sep_id):
-    """ Dynamically build the segment IDs for a concatenated pair of sentences
-    Searches for index SEP_ID in the tensor
-
-    args:
-        ids (torch.LongTensor): batch of token IDs
-
-    returns:
-        seg_ids (torch.LongTensor): batch of segment IDs
-
-    example:
-    > sents = ["[CLS]", "I", "am", "a", "cat", ".", "[SEP]", "You", "like", "cats", "?", "[SEP]"]
-    > token_tensor = torch.Tensor([[vocab[w] for w in sent]]) # a tensor of token indices
-    > seg_ids = _get_seg_ids(token_tensor, sep_id=102) # BERT [SEP] ID
-    > assert seg_ids == torch.LongTensor([0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
-    """
-    sep_idxs = (ids == sep_id).nonzero()[:, 1]
-    assert sep_idxs.size(1) == 2
-    seg_ids = torch.ones_like(ids)
-    for row, idx in zip(seg_ids, sep_idxs[::2]):
-        row[:idx + 1].fill_(0)
-    return seg_ids
-
-
 class BertEmbedderModule(nn.Module):
     """ Wrapper for BERT module to fit into jiant APIs. """
 
