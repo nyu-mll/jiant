229e1e7f93e478b6fe8d92b1e8633c4af1bf175b
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-04-12 17:09:34 -0400

    conf files for separating pretraining and target-task training

---
+++ npi_probing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=20:00:00
+#SBATCH --job-name=npi_probing
+#SBATCH --mail-type=END
+##SBATCH --mail-user=sfw268@nyu.edu
+#SBARCH --mem=320GB
+#SBATCH --output=slurm_%j.out
+#SBATCH --gres=gpu:1
+
+
+
+python main.py --config_file config/npi_probing_finetune.conf --overrides "exp_name=npi_probing_test, run_name = run_01, bert_fine_tune = 1, do_pretrain = 1, pretrain_tasks = cola_npi, do_target_task_training = 0, do_full_eval = 0"
+
+python main.py --config_file config/npi_probing_target_task.conf --overrides "exp_name=npi_probing_test, run_name = run_01, bert_fine_tune = 0, do_pretrain = 0, do_target_task_training = 1, do_full_eval = 1"
+
