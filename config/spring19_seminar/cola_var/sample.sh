1ff0df9d9f7d97d2fa7fb810ec9a034e662db1c3
PteroDiacos <pterosdiacos@outlook.com>
2019-04-19 17:25:22 -0400

    update

--- config/spring19_seminar/cola_var/sample.sh
+++ config/spring19_seminar/cola_var/sample.sh
@@ -5,30 +5,30 @@
 #SBATCH --job-name=myrun
 #SBATCH --output=slurm_%j.out
 
-python main.py --config_file cola_confs/cola_bert.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bert_ccg, target_tasks = "cola", pretrain_tasks = "ccg", do_pretrain = 1" 
+python main.py --config_file config/spring19_seminar/bert.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bert_ccg_cola, target_tasks = "cola", pretrain_tasks = "ccg", do_pretrain = 1" 
 
-python main.py --config_file cola_confs/cola_bert.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bert_mnli, target_tasks = "cola", pretrain_tasks = "mnli", do_pretrain = 1" 
+python main.py --config_file config/spring19_seminar/bert.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bert_mnli_cola, target_tasks = "cola", pretrain_tasks = "mnli", do_pretrain = 1" 
 
-python main.py --config_file cola_confs/cola_bert.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bert_none, target_tasks = "cola", pretrain_tasks = "none", do_pretrain = 0, allow_untrained_encoder_parameters = 1" 
+python main.py --config_file config/spring19_seminar/bert.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bert_none_cola, target_tasks = "cola", pretrain_tasks = "none", do_pretrain = 0, allow_untrained_encoder_parameters = 1" 
 
-python main.py --config_file cola_confs/cola_bilstm.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bilstm_ccg, target_tasks = "cola", pretrain_tasks = "ccg", do_pretrain = 1" 
+python main.py --config_file config/spring19_seminar/bilstm.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bilstm_ccg_cola, target_tasks = "cola", pretrain_tasks = "ccg", do_pretrain = 1" 
 
-python main.py --config_file cola_confs/cola_bilstm.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bilstm_mnli, target_tasks = "cola", pretrain_tasks = "mnli", do_pretrain = 1" 
+python main.py --config_file config/spring19_seminar/bilstm.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bilstm_mnli_cola, target_tasks = "cola", pretrain_tasks = "mnli", do_pretrain = 1" 
 
-python main.py --config_file cola_confs/cola_bilstm.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bilstm_none, target_tasks = "cola", pretrain_tasks = "none", do_pretrain = 0, allow_untrained_encoder_parameters = 1" 
+python main.py --config_file config/spring19_seminar/bilstm.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bilstm_none_cola, target_tasks = "cola", pretrain_tasks = "none", do_pretrain = 0, allow_untrained_encoder_parameters = 1" 
 
-python main.py --config_file cola_confs/cola_bow_glove.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bow_glove_ccg, target_tasks = "cola", pretrain_tasks = "ccg", do_pretrain = 1" 
+python main.py --config_file config/spring19_seminar/bow_glove.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bow_glove_ccg_cola, target_tasks = "cola", pretrain_tasks = "ccg", do_pretrain = 1" 
 
-python main.py --config_file cola_confs/cola_bow_glove.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bow_glove_mnli, target_tasks = "cola", pretrain_tasks = "mnli", do_pretrain = 1" 
+python main.py --config_file config/spring19_seminar/bow_glove.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bow_glove_mnli_cola, target_tasks = "cola", pretrain_tasks = "mnli", do_pretrain = 1" 
 
-python main.py --config_file cola_confs/cola_bow_glove.conf \
-    --overrides "exp_name = exp_pretrain, run_name = run_bow_glove_none, target_tasks = "cola", pretrain_tasks = "none", do_pretrain = 0, allow_untrained_encoder_parameters = 1" 
+python main.py --config_file config/spring19_seminar/bow_glove.conf \
+    --overrides "exp_name = npi_acpt, run_name = run_bow_glove_none_cola, target_tasks = "cola", pretrain_tasks = "none", do_pretrain = 0, allow_untrained_encoder_parameters = 1" 
 
