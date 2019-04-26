0d1a592c7bb31e2de71222c2c70c3debc226675b
anhad13 <anhadmohananey@gmail.com>
2019-04-26 13:56:47 -0400

    Updates

--- export_from_bashrc.sh
+++ export_from_bashrc.sh
@@ -1,3 +1,6 @@
+# Running 'source export_from_bashrc.sh' will add running path_config.sh directly to bashrc
+# for automatic updates on booting up the linux terminal.
+
 function changebashpaths() {
 	output="source path_config.sh"
 	echo $output >> ~/.bashrc
