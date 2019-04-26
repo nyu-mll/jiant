4870e894afffffbc066de092195eeb4bf003a077
Sam Bowman <sbowman@stanford.edu>
2019-04-26 15:46:52 -0400

    Partial cleanup.

--- path_config.sh
+++ path_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# DO NOT COMMIT CHANGES TO THIS FILE! Make a local copy and follow the
-# instructions below.
+# DO NOT COMMIT CHANGES TO THIS FILE! 
+# Make a local copy and follow the instructions below.
 
 # Copy this to /etc/profile.d/ to auto-set environment vars on login.
 # Or, make a copy of this, customize, and run immediately before the training
@@ -11,21 +11,16 @@
 #   --overrides "do_pretrain = 0"
 
 # Default environment variables for JSALT code. May be overwritten by user.
-# See https://github.com/jsalt18-sentence-repl/jiant for more.
+# See https://github.com/nyu-mll/jiant for more.
 
 ##
 # Example of custom paths for a local installation:
 # export JIANT_PROJECT_PREFIX=/Users/Bowman/Drive/JSALT
 # export JIANT_DATA_DIR=/Users/Bowman/Drive/JSALT/jiant/glue_data
-# export JIANT_DATA_DIR=/home/raghu1991_p_gmail_com/
 # export WORD_EMBS_FILE=~/glove.840B.300d.txt
-# export FASTTEXT_MODEL_FILE=None
 # export FASTTEXT_EMBS_FILE=None
 
-export JIANT_PROJECT_PREFIX=exp
-export JIANT_DATA_DIR=/nfs/jsalt/home/
-export NFS_PROJECT_PREFIX=/nfs/jsalt/exp/nkim
-export NFS_DATA_DIR=/nfs/jsalt/home/
-
-# echo "Loaded Sam's config."
-
+export JIANT_PROJECT_PREFIX=~
+export JIANT_DATA_DIR=~
+export WORD_EMBS_FILE=None
+export FASTTEXT_EMBS_FILE=None
