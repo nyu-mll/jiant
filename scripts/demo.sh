e91e6c215fba8cc18bbf4686f2828df4fdadaa45
Sam Bowman <sbowman@stanford.edu>
2019-04-30 14:54:00 -0400

    Path cleanup

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Quick-start: run this from the base jiant/ directory
+# Quick-start: set up path_config.sh, run it, then run this
+pushd "${PWD%jiant*}jiant"  # Make sure we're in the base jiant directory
 python main.py --config_file config/demo.conf
