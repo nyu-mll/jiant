3234f9bf3597e484b19d5962f54bc5eb49eee71b
Jason Phang <email@jasonphang.com>
2019-04-30 14:35:41 -0400

    Clean up tests

--- tests/test_ccg_allignment.py
+++ tests/test_ccg_allignment.py
@@ -1,14 +1,17 @@
-import unittest
 import csv
-import jiant.scripts.ccg.align_tags_to_bert as ccg_aligner
+import os
+import shutil
+import tempfile
+import unittest
+import scripts.ccg.align_tags_to_bert as ccg_aligner
 from string import ascii_lowercase
-
-TEMP_DATASET_CCG = "temp_ccg_dataset.tsv"
 
 
 class TestCCGAllignment(unittest.TestCase):
     def setUp(self):
-        with open(TEMP_DATASET_CCG, 'w') as tsvfile:
+        self.temp_dir = tempfile.mkdtemp()
+        self.path = os.path.join(self.temp_dir, "temp_ccg_dataset.tsv")
+        with open(self.path, 'w') as tsvfile:
             writer = csv.writer(tsvfile, delimiter='\t')
             writer.writerow(["text", "tags"])
             # test for a text that shouldn't be changed by tokenization
@@ -23,9 +26,8 @@ class TestCCGAllignment(unittest.TestCase):
                 ["if more information Mr. Ford is needed from you", "E F G H I J K L M"])
 
     def test(self):
-        max_seq_len = 30
         import pandas as pd
-        file = pd.read_csv(TEMP_DATASET_CCG, sep="\t")
+        file = pd.read_csv(self.path, sep="\t")
         tag_ids = {ascii_lowercase[i].upper(): i for i in range(len((list(ascii_lowercase))))}
         result = ccg_aligner.align_tags_BERT(file, "bert-large-uncased", tag_ids)
         assert result.iloc[0]["text"] == file.iloc[0]["text"]
@@ -36,3 +38,6 @@ class TestCCGAllignment(unittest.TestCase):
             " ") == ['4', '26', '5', '6', '7', '8', '9', '10', '11', '12']
         assert result.iloc[3]["tags"].split(
             " ") == ['4', '5', '6', '7', '26', '8', '9', '10', '11', '12']
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
