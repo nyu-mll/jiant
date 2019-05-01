54027f6c50db6130aa43e930e3c6f3fa4b490572
Jason Phang <email@jasonphang.com>
2019-05-01 01:25:47 -0400

    PEP8

--- tests/test_ccg_allignment.py
+++ tests/test_ccg_allignment.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import pandas as pd
 import shutil
 import tempfile
 import unittest
@@ -7,7 +8,7 @@ import scripts.ccg.align_tags_to_bert as ccg_aligner
 from string import ascii_lowercase
 
 
-class TestCCGAllignment(unittest.TestCase):
+class TestCCGAlignment(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
         self.path = os.path.join(self.temp_dir, "temp_ccg_dataset.tsv")
@@ -15,21 +16,32 @@ class TestCCGAllignment(unittest.TestCase):
             writer = csv.writer(tsvfile, delimiter='\t')
             writer.writerow(["text", "tags"])
             # test for a text that shouldn't be changed by tokenization
-            writer.writerow(["Influential members of the House", "A B C D E "])
+            writer.writerow([
+                "Influential members of the House", "A B C D E ",
+            ])
             # test for text that should be changed at the end
-            writer.writerow(
-                ["If more information is needed from you Mr. Ford ", "E F G H I J K L M"])
+            writer.writerow([
+                "If more information is needed from you Mr. Ford ",
+                "E F G H I J K L M",
+            ])
             # text for text that should be changed at the beginning
-            writer.writerow(
-                ["Mr. Ford if more information is needed from you", "E F G H I J K L M"])
-            writer.writerow(
-                ["if more information Mr. Ford is needed from you", "E F G H I J K L M"])
+            writer.writerow([
+                "Mr. Ford if more information is needed from you",
+                "E F G H I J K L M",
+            ])
+            writer.writerow([
+                "if more information Mr. Ford is needed from you",
+                "E F G H I J K L M",
+            ])
 
     def test(self):
-        import pandas as pd
         file = pd.read_csv(self.path, sep="\t")
-        tag_ids = {ascii_lowercase[i].upper(): i for i in range(len((list(ascii_lowercase))))}
-        result = ccg_aligner.align_tags_BERT(file, "bert-large-uncased", tag_ids)
+        tag_ids = {
+            ascii_lowercase[i].upper(): i
+            for i in range(len((list(ascii_lowercase))))
+        }
+        result = ccg_aligner.align_tags_BERT(
+            file, "bert-large-uncased", tag_ids)
         assert result.iloc[0]["text"] == file.iloc[0]["text"]
         assert result.iloc[0]["tags"].split(" ") == ["0", "1", "2", "3", "4"]
         assert result.iloc[1]["tags"].split(
