9d946297b00e514f44a92b0b52d69586f695fae8
Anhad Mohananey <anhadmohananey@gmail.com>
2019-05-02 18:02:44 -0400

    changes

--- user_config_template.sh
+++ user_config_template.sh
@@ -20,7 +20,6 @@
 # export WORD_EMBS_FILE=~/glove.840B.300d.txt  
 # The base directory for model output.
 export JIANT_PROJECT_PREFIX=~
-
 # Base directory in which to look for raw data subdirectories. This
 # could be the glue_data directory created by download_glue_data.py.
 export JIANT_DATA_DIR=~
