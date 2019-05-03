0189a93b05e49b60777031dfdf9ac75b8a41512e
w4ngatang <wang.alex.c@gmail.com>
2019-05-02 21:35:11 -0400

    remove get task whitelist

--- src/models.py
+++ src/models.py
@@ -300,24 +300,6 @@ def build_model(args, vocab, pretrained_embs, tasks):
     return model
 
 
-def get_task_whitelist(args):
-    """Filters tasks so that we only build models that we will use, meaning we only
-    build models for train tasks and for classifiers of eval tasks"""
-    eval_task_names = parse_task_list_arg(args.target_tasks)
-    eval_clf_names = []
-    for task_name in eval_task_names:
-        override_clf = config.get_task_attr(args, task_name, "use_classifier")
-        if override_clf == "none" or override_clf is None:
-            eval_clf_names.append(task_name)
-        else:
-            eval_clf_names.append(override_clf)
-    train_task_names = parse_task_list_arg(args.pretrain_tasks)
-    log.info(
-        "Whitelisting train tasks=%s, eval_clf_tasks=%s", str(train_task_names), str(eval_clf_names)
-    )
-    return train_task_names, eval_clf_names
-
-
 def build_embeddings(args, vocab, tasks, pretrained_embs=None):
     """ Build embeddings according to options in args """
     d_emb, d_char = 0, args.d_char
