d01211b3c48e7370afbba9bb2f4def9a6c4b4466
Sam Bowman <sbowman@stanford.edu>
2019-05-03 17:00:55 -0400

    Format fix.

--- src/models.py
+++ src/models.py
@@ -298,9 +298,9 @@ def build_model(args, vocab, pretrained_embs, tasks):
                     "\t%s: Trainable parameter, count %d with %s", name, np.prod(param.size()), str(param.size())
                 )
         elif args.list_params:
-                log.info(
-                    "\t%s: Non-trainable parameter, count %d with %s", name, np.prod(param.size()), str(param.size())
-                )
+            log.info(
+                "\t%s: Non-trainable parameter, count %d with %s", name, np.prod(param.size()), str(param.size())
+            )
     log.info("Total number of parameters: {ct:d} ({ct:g})".format(ct=param_count))
     log.info("Number of trainable parameters: {ct:d} ({ct:g})".format(ct=trainable_param_count))
     return model
