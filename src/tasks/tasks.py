f32931e6407eb037fa04077ed7def17f1cfcb7f9
Yada Pruksachatkun <yp913@cassio.cs.nyu.edu>
2019-05-23 04:34:11 -0400

    black reformatting

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -2514,11 +2514,7 @@ class SWAGTask(MultipleChoiceTask):
 @register_task("winograd-coreference", rel_path="winograd-coref")
 class WinogradCoreferenceTask(SpanClassificationTask):
     def __init__(self, path, **kw):
-        self._files_by_split = {
-            "train": "train.jsonl",
-            "val": "val.jsonl",
-            "test": "test.jsonl",
-        }
+        self._files_by_split = {"train": "train.jsonl", "val": "val.jsonl", "test": "test.jsonl"}
         self.num_spans = 2
         super().__init__(
             files_by_split=self._files_by_split, label_file="labels.txt", path=path, **kw
