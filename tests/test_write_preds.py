4c16d05b44345f8225bd7462355f5fc54d07e96f
pruksmhc <pruks22y@mtholyoke.edu>
2019-06-12 15:37:44 -0400

    new_2

--- tests/test_write_preds.py
+++ tests/test_write_preds.py
@@ -44,9 +44,7 @@ class TestWritePreds(unittest.TestCase):
         """
         self.temp_dir = tempfile.mkdtemp()
         self.temp_dir = '~/repo'+ self.temp_dir
-        os.mkdir("~/repo")
-        os.mkdir("~/repo/tmp")
-        os.mkdir(self.temp_dir)
+        os.makedirs(self.temp_dir)
         self.path = os.path.join(self.temp_dir, "temp_dataset.tsv")
         self.stsb = tasks.STSBTask(self.temp_dir, 100, "sts-b", tokenizer_name="MosesTokenizer")
         self.wic = tasks.WiCTask(self.temp_dir, 100, "wic", tokenizer_name="MosesTokenizer")
