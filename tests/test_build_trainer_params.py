b18882bd6ff049c04e926201df0859bf8a03efc0
Sam Bowman <bowman@nyu.edu>
2019-06-27 13:32:13 -0400

    Delete print, add check from defaults

--- tests/test_build_trainer_params.py
+++ tests/test_build_trainer_params.py
@@ -30,12 +30,12 @@ class TestBuildTrainerParams(unittest.TestCase):
         self.processed_qqp_target_params = build_trainer_params(
             params, ["qqp"], phase="target_train"
         )
-        print(self.processed_pretrain_params["lr"])
 
     def test_pretrain_task_specific(self):
         # Task specific trainer parameters shouldn't apply during pretraining.
         assert self.processed_pretrain_params["lr"] == 123.456
         assert self.processed_pretrain_params["training_data_fraction"] == 0.123
+        assert self.processed_pretrain_params["keep_all_checkpoints"] == 0  # From defaults
 
     def test_target_task_specific(self):
         # Target task parameters should be task specific when possible, and draw on defaults otherwise.
@@ -45,3 +45,5 @@ class TestBuildTrainerParams(unittest.TestCase):
         assert self.processed_qqp_target_params["max_epochs"] == 789
         assert self.processed_mnli_target_params["training_data_fraction"] == 0.456
         assert self.processed_qqp_target_params["training_data_fraction"] == 0.1234
+        assert self.processed_mnli_target_params["keep_all_checkpoints"] == 0  # From defaults
+        assert self.processed_qqp_target_params["keep_all_checkpoints"] == 0  # From defaults
