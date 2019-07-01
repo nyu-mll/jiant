069c33cf68fa572f6c6c350c4ab6d3afe62c6a67
w4ngatang <wang.alex.c@gmail.com>
2019-07-01 11:17:19 -0400

    Add official versions of f1 nad eM

--- src/tasks/qa.py
+++ src/tasks/qa.py
@@ -16,6 +16,47 @@ from .tasks import Task
 from .tasks import sentence_to_text_field
 from .registry import register_task
 
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+    def remove_articles(text):
+        return re.sub(r'\b(a|an|the)\b', ' ', text)
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def f1_score(prediction, ground_truth):
+    prediction_tokens = normalize_answer(prediction).split()
+    ground_truth_tokens = normalize_answer(ground_truth).split()
+    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+    num_same = sum(common.values())
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(prediction_tokens)
+    recall = 1.0 * num_same / len(ground_truth_tokens)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+
+def exact_match_score(prediction, ground_truth):
+    return normalize_answer(prediction) == normalize_answer(ground_truth)
+
+
+def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
+    scores_for_ground_truths = []
+    for ground_truth in ground_truths:
+        score = metric_fn(prediction, ground_truth)
+        scores_for_ground_truths.append(score)
+    return max(scores_for_ground_truths)
 
 def _get_f1(x, y):
     """ """
@@ -367,11 +408,13 @@ class ReCoRDTask(Task):
             pred = anss[pred]
 
             # F1
-            f1 = max([_get_f1(pred, gold) for gold in golds])
+            #f1 = max([_get_f1(pred, gold) for gold in golds])
+            f1 = metric_max_over_ground_truths(f1_score, pred, golds)
             f1s.append(f1)
 
             # EM
-            em = max([int(pred == gold) for gold in golds])
+            #em = max([int(pred == gold) for gold in golds])
+            em = metric_max_over_ground_truths(exact_match_score, pred, golds)
             ems.append(em)
 
         em = sum(ems) / len(ems)
