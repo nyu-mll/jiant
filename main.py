a205e6ef1b69d697e19dd026b596b78dc3086562
w4ngatang <wang.alex.c@gmail.com>
2019-07-07 21:50:43 -0400

    Rename accuracy; fix preprocessing bug

--- main.py
+++ main.py
@@ -285,11 +285,12 @@ def evaluate_and_write(args, model, tasks, splits_to_write):
             tasks, val_preds, args.run_dir, "val", strict_glue_format=args.write_strict_glue_format
         )
     if "test" in splits_to_write:
-        _, te_preds = evaluate.evaluate(model, tasks, args.batch_size, args.cuda, "test")
+        te_results, te_preds = evaluate.evaluate(model, tasks, args.batch_size, args.cuda, "test")
         evaluate.write_preds(
             tasks, te_preds, args.run_dir, "test", strict_glue_format=args.write_strict_glue_format
         )
 
+    import ipdb; ipdb.set_trace()
     run_name = args.get("run_name", os.path.basename(args.run_dir))
     results_tsv = os.path.join(args.exp_dir, "results.tsv")
     log.info("Writing results for split 'val' to %s", results_tsv)
