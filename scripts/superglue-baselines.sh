ca831f650f7e1f6e0068130a4853986fc8691303
w4ngatang <wang.alex.c@gmail.com>
2019-07-09 02:12:20 -0400

    Cleanup command

--- scripts/superglue-baselines.sh
+++ scripts/superglue-baselines.sh
@@ -15,7 +15,7 @@ function boolq() {
 }
 
 function commit() {
-    python main.py --config config/superglue-bert.conf --overrides "random_seed = ${seed}, cuda = ${gpuid}, run_name = commitbank, pretrain_tasks = \"commitbank\", target_tasks = \"commitbank\", do_pretrain = 1, do_target_task_training = 0, do_full_eval = 1, batch_size = 4, val_interval = 60, write_strict_glue_format = 0"
+    python main.py --config config/superglue-bert.conf --overrides "random_seed = ${seed}, cuda = ${gpuid}, run_name = commitbank, pretrain_tasks = \"commitbank\", target_tasks = \"commitbank\", do_pretrain = 1, do_target_task_training = 0, do_full_eval = 1, batch_size = 4, val_interval = 60"
 }
 
 function copa() {
