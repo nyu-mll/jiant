5a2171ed78601a1e0a8e5288d53163554c996d94
Alex Wang <wangalexc@gmail.com>
2019-07-08 22:14:59 -0400

    dev 2 val in 2 tasks (#824)

--- jiant/tasks/tasks.py
+++ jiant/tasks/tasks.py
@@ -2711,7 +2711,7 @@ class BooleanQuestionTask(PairClassificationTask):
             return data
 
         self.train_data_text = _load_jsonl(os.path.join(self.path, "train.jsonl"))
-        self.val_data_text = _load_jsonl(os.path.join(self.path, "dev.jsonl"))
+        self.val_data_text = _load_jsonl(os.path.join(self.path, "val.jsonl"))
         self.test_data_text = _load_jsonl(os.path.join(self.path, "test.jsonl"))
 
         self.sentences = [d["question"] for d in self.train_data_text + self.val_data_text] + [
