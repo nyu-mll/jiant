ddf45f71b49d93841a7d4ca9593d320bbf1863e3
Katharina Kann <katharinakann@10-19-236-138.dynapool.wireless.nyu.edu>
2019-09-05 13:42:08 -0400

    directly import beam search from AllenNLP

--- jiant/modules/seq2seq_decoder.py
+++ jiant/modules/seq2seq_decoder.py
@@ -10,6 +10,7 @@ from allennlp.data.vocabulary import Vocabulary
 from allennlp.models.model import Model
 from allennlp.modules.attention import BilinearAttention
 from allennlp.modules.token_embedders import Embedding
+from allennlp.nn.beam_search import BeamSearch
 from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits, weighted_sum
 from overrides import overrides
 from torch.nn.functional import log_softmax
@@ -18,7 +19,6 @@ from torch.nn.modules.rnn import LSTMCell
 
 from jiant.modules.simple_modules import Pooler
 from jiant.modules.attention import BahdanauAttention
-from jiant.modules.beam_search import BeamSearch
 from jiant.preprocess import SOS_TOK, EOS_TOK, UNK_TOK
 
 
