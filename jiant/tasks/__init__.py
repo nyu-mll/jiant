85a697e443211498791508f68cbe75582003d383
Haokun Liu <haokunliu412@gmail.com>
2019-09-06 17:38:52 -0400

    Update __init__.py

--- jiant/tasks/__init__.py
+++ jiant/tasks/__init__.py
@@ -1,5 +1,5 @@
 # Import task definitions to register their tasks.
-from jiant.tasks import edge_probing, lm, nli_probing, qa, seq2seq, tasks
+from jiant.tasks import edge_probing, lm, nli_probing, tasks, qa, seq2seq, acceptablity
 
 # REGISTRY needs to be available to modules within this package,
 # but we also import it here to make it available at the package level.
