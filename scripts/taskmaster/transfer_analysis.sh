a019bf95d558383b09edb713eb17e81bd3a63789
Haokun Liu <haokunliu412@gmail.com>
2019-11-12 14:22:47 -0500

    update script

--- scripts/taskmaster/transfer_analysis.sh
+++ scripts/taskmaster/transfer_analysis.sh
@@ -90,9 +90,9 @@ function run_intermediate_to_target_task() {
     # This function can also be used to finetune on a probing task as well.
     # Usage: run_intermediate_to_target_task <intemediate_task> <target_task> <directory_to_project_dir> <config_number> <batch_size>
     OVERRIDES="exp_name=$1, run_name=$2"
-    OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/$1/model_*.best.th, pretrain_tasks=$2,"
-    OVERRIDES+="input_module=roberta-large, batch_size=$5"
-    OVERRIDES+="do_pretrain=0, do_target_task_training=1"
+    OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/$1/model_*.best.th, pretrain_tasks=\"\""
+    OVERRIDES+=", input_module=roberta-large, batch_size=$5"
+    OVERRIDES+=", do_pretrain=0, do_target_task_training=1"
     run_exp "jiant/config/taskmaster/base_roberta.conf" "${OVERRIDES}" ${4}
 }
 
@@ -100,9 +100,9 @@ function run_intermediate_to_edgeprobing() {
     # Using a pretrained intermediate task, finetune on an edge probing task.
     # Usage: run_intermediate_to_target_task <intemeidate_task> <edge_probing task> <directory_to_project_dir> <batch_size>
     OVERRIDES="exp_name=$1, run_name=$2"
-    OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/$1/model_*.best.th, pretrain_tasks=\"\","
-    OVERRIDES+="input_module=roberta-large, batch_size=$5"
-    OVERRIDES+="do_pretrain=0, do_target_task_training=1"
+    OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/$1/model_*.best.th, pretrain_tasks=\"\""
+    OVERRIDES+=", input_module=roberta-large, batch_size=$5"
+    OVERRIDES+=", do_pretrain=0, do_target_task_training=1"
     run_exp "jiant/config/taskmaster/base_edgeprobe.conf" "${OVERRIDES}" ${4}
 }
 
