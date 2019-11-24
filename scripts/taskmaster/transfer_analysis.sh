3218a88269008bab30ae2475c37ba3918154b790
Yada Pruksachatkun <yp913@nyu.edu>
2019-11-24 16:55:07 -0500

    adding config to hyperparameter sweep

--- scripts/taskmaster/transfer_analysis.sh
+++ scripts/taskmaster/transfer_analysis.sh
@@ -74,7 +74,6 @@ TASK_TYPE_MAP["cola"]="regular"
 #########################################
 # Hyperparameter tuning experiments
 #########################################
-
 function hyperparameter_sweep() {
     # Do hyerparameter tuning search for the parameters
     # Usage: hyperparameter_sweep <task> <batch_size>
@@ -83,7 +82,13 @@ function hyperparameter_sweep() {
     for i in 0 1 2 3 4 5 6 7
     do
         EXP_OVERRIDES="${OVERRIDES}, run_name=$1config$i"
-        run_exp "jiant/config/taskmaster/base_roberta.conf" "${EXP_OVERRIDES}" $i
+        TASK_TYPE=${TASK_TYPE_MAP[$1]}
+        if [ ${TASK_TYPE} == "edge" ]; then
+            BASE_CONFIG_FILE="base_edgeprobe"
+        elif [ ${TASK_TYPE} == "regular" ]; then
+            BASE_CONFIG_FILE="base_roberta"
+        fi
+        run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${EXP_OVERRIDES}" $i
     done
 
 }
@@ -96,7 +101,13 @@ function hyperparameter_sweep_mix() {
     for i in 0 1 2 3 4 5 6 7 8 9
     do
         EXP_OVERRIDES="${OVERRIDES}, run_name=$1configmix$i"
-        run_exp "jiant/config/taskmaster/base_roberta.conf" "${EXP_OVERRIDES}" $i
+        TASK_TYPE=${TASK_TYPE_MAP[$1]}
+        if [ ${TASK_TYPE} == "edge" ]; then
+            BASE_CONFIG_FILE="base_edgeprobe"
+        elif [ ${TASK_TYPE} == "regular" ]; then
+            BASE_CONFIG_FILE="base_roberta"
+        fi
+        run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${EXP_OVERRIDES}" $i
     done
 
 }
