fc7e70449bef8410b1819c451baa778c8acc67a5
Jason Phang <email@jasonphang.com>
2019-11-25 09:26:52 -0500

    Taskmaster seed (#969)
    * adding seed
    
    * modifying parsing
    
    * typo

--- scripts/taskmaster/transfer_analysis.sh
+++ scripts/taskmaster/transfer_analysis.sh
@@ -13,9 +13,11 @@ function run_exp() {
     # Helper function to invoke main.py.
     # Don't run this directly - use the experiment functions below,
     # or create a new one for a new experiment suite.
-    # Usage: run_exp <config_file> <overrides>
+    # Usage: run_exp <config_file> <overrides> <hparam_set> <random_seed>
     CONFIG_FILE=$1
     OVERRIDES=$2
+    RANDOM_SEED=${4:-1234}
+
     if [ $3 == 0 ]; then
         OVERRIDES+=", lr=2e-5, dropout=0.2"
     elif [ $3 == 1 ]; then
@@ -37,9 +39,16 @@ function run_exp() {
     elif [ $3 == 9 ]; then
         OVERRIDES+=", lr=1e-3, dropout=0.1"
     fi
+
+    # Add random seed
+    OVERRIDES+=", random_seed=${RANDOM_SEED}"
+
+    # Construct args
     declare -a args
     args+=( --config_file "${CONFIG_FILE}" )
     args+=( -o "${OVERRIDES}" )
+
+    # Run
     python main.py "${args[@]}"
 }
 
@@ -76,7 +85,7 @@ TASK_TYPE_MAP["cola"]="regular"
 #########################################
 function hyperparameter_sweep() {
     # Do hyerparameter tuning search for the parameters
-    # Usage: hyperparameter_sweep <task> <batch_size>
+    # Usage: hyperparameter_sweep <task> <batch_size> <random_seed>
     OVERRIDES="exp_name=roberta-large"
     OVERRIDES+=", target_tasks=$1, do_pretrain=0, batch_size=$2, reload_vocab=1, do_target_task_training=1, input_module=roberta-large,pretrain_tasks=\"\""
     for i in 0 1 2 3 4 5 6 7
@@ -88,14 +97,14 @@ function hyperparameter_sweep() {
         elif [ ${TASK_TYPE} == "regular" ]; then
             BASE_CONFIG_FILE="base_roberta"
         fi
-        run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${EXP_OVERRIDES}" $i
+        run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${EXP_OVERRIDES}" $i $3
     done
 
 }
 
 function hyperparameter_sweep_mix() {
     # Do hyerparameter tuning search for the parameters
-    # Usage: hyperparameter_sweep <task> <batch_size>
+    # Usage: hyperparameter_sweep <task> <batch_size> <random_seed>
     OVERRIDES="exp_name=roberta-large"
     OVERRIDES+=", target_tasks=$1, do_pretrain=0, batch_size=$2, reload_vocab=1, transfer_paradigm=frozen, allow_untrained_encoder_parameters=1, pytorch_transformers_output_mode = mix,do_target_task_training=1, input_module=roberta-large,pretrain_tasks=\"\""
     for i in 0 1 2 3 4 5 6 7 8 9
@@ -107,7 +116,7 @@ function hyperparameter_sweep_mix() {
         elif [ ${TASK_TYPE} == "regular" ]; then
             BASE_CONFIG_FILE="base_roberta"
         fi
-        run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${EXP_OVERRIDES}" $i
+        run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${EXP_OVERRIDES}" $i $3
     done
 
 }
@@ -117,27 +126,27 @@ function hyperparameter_sweep_mix() {
 #########################################
 
 function first_intermediate_exp() {
-    # Initial intermdiate task pretraining.
-    # Usage: first_intermediate_task <intermediate_task_name> <config_number> <batch_size>
+    # Initial intermediate task pretraining.
+    # Usage: first_intermediate_task <intermediate_task_name> <config_number> <batch_size> <random_seed>
     OVERRIDES="exp_name=roberta-large, run_name=$1, batch_size=$3, reload_vocab=1"
     OVERRIDES+=", target_tasks=\"\", do_pretrain=1, do_target_task_training=0, input_module=roberta-large,pretrain_tasks=$1"
-    run_exp "jiant/config/taskmaster/base_roberta.conf" "${OVERRIDES}" ${2}
+    run_exp "jiant/config/taskmaster/base_roberta.conf" "${OVERRIDES}" ${2} ${4}
 }
 
 function run_intermediate_to_target_task() {
     # Using a pretrained intermediate task, finetune on a target task.  ("STILTs" sheet)
     # This function can also be used to finetune on a probing task as well.
-    # Usage: run_intermediate_to_target_task <intermediate_task> <target_task> <directory_to_project_dir> <config_number> <batch_size>
+    # Usage: run_intermediate_to_target_task <intermediate_task> <target_task> <directory_to_project_dir> <config_number> <batch_size> <random_seed>
     OVERRIDES="exp_name=$1, run_name=$2"
     OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/model_*.best.th, pretrain_tasks=\"\""
     OVERRIDES+=", input_module=roberta-large, batch_size=$5, reload_vocab=1"
     OVERRIDES+=", do_pretrain=0, do_target_task_training=1"
-    run_exp "jiant/config/taskmaster/base_roberta.conf" "${OVERRIDES}" ${4}
+    run_exp "jiant/config/taskmaster/base_roberta.conf" "${OVERRIDES}" ${4} ${5}
 }
 
 function run_intermediate_to_probing() {
     # Using a pretrained intermediate task, finetune on an probing task.  ("Probing" sheet)
-    # Usage: run_intermediate_to_probing <intermediate_task> <probing task> <directory_to_project_dir> <config_number> <batch_size>
+    # Usage: run_intermediate_to_probing <intermediate_task> <probing task> <directory_to_project_dir> <config_number> <batch_size> <random_seed>
     OVERRIDES="exp_name=$1, run_name=$2"
     OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/$1/model_*.best.th, pretrain_tasks=\"\""
     OVERRIDES+=", input_module=roberta-large, batch_size=$5, reload_vocab=1"
@@ -148,18 +157,16 @@ function run_intermediate_to_probing() {
     elif [ ${TASK_TYPE} == "regular" ]; then
         BASE_CONFIG_FILE="base_roberta"
     fi
-    run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${OVERRIDES}" ${4}
+    run_exp "jiant/config/taskmaster/${BASE_CONFIG_FILE}.conf" "${OVERRIDES}" ${4} ${6}
 }
-
-
 
 function run_intermediate_to_mixing() {
     # Using a pretrained intermediate task, use frozen encoder with mixing on an probing task.  ("Mixing" sheet)
-    # Usage: run_intermediate_to_mixing <intermediate_task> <probing task> <directory_to_project_dir> <config_number> <batch_size>
+    # Usage: run_intermediate_to_mixing <intermediate_task> <probing task> <directory_to_project_dir> <config_number> <batch_size> <random_seed>
     OVERRIDES="exp_name=$1, run_name=$2_mix"
     OVERRIDES+=", target_tasks=$2, load_model=1, load_target_train_checkpoint=$3/roberta-large/$1/model_*.best.th, pretrain_tasks=\"\""
     OVERRIDES+=", input_module=roberta-large, batch_size=$5, reload_vocab=1"
     OVERRIDES+=", transfer_paradigm=frozen, allow_untrained_encoder_parameters=1, pytorch_transformers_output_mode = mix"
     OVERRIDES+=", do_pretrain=0, do_target_task_training=1"
-    run_exp "jiant/config/taskmaster/base_edgeprobe.conf" "${OVERRIDES}" ${4}
+    run_exp "jiant/config/taskmaster/base_edgeprobe.conf" "${OVERRIDES}" ${4} ${6}
 }
