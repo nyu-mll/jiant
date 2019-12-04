7704fcce5ab1fe2346c347d567f2cdb8b6652d71
pruksmhc <pruks22y@mtholyoke.edu>
2019-12-04 08:34:28 -0500

    fix vanishing loss problem

--- jiant/trainer.py
+++ jiant/trainer.py
@@ -607,7 +607,6 @@ class SamplingMultiTaskTrainer:
                     output_dict, "loss", self._cuda_device
                 )  # optionally scale loss
                 loss *= scaling_weights[task.name]
-                loss /= self._accumulation_steps
                 loss.backward()
                 assert_for_log(not torch.isnan(loss).any(), "NaNs in loss.")
                 tr_loss += loss.data.cpu().numpy()
