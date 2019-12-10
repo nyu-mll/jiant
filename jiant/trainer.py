a78444d0d1b1522dd1cedcaec32810b2fec4973f
pyeres <py1@uw.edu>
2019-12-10 11:58:36 -0500

    add loss normalization

--- jiant/trainer.py
+++ jiant/trainer.py
@@ -606,10 +606,12 @@ class SamplingMultiTaskTrainer:
                 loss = get_output_attribute(
                     output_dict, "loss", self._cuda_device
                 )  # optionally scale loss
+                if self._accumulation_steps > 1:
+                    loss = loss / self._accumulation_steps
                 loss *= scaling_weights[task.name]
                 loss.backward()
                 assert_for_log(not torch.isnan(loss).any(), "NaNs in loss.")
-                tr_loss += loss.data.cpu().numpy()
+                tr_loss += loss.data.cpu().numpy() * self._accumulation_steps
 
                 n_step += 1  # update per batch
 
