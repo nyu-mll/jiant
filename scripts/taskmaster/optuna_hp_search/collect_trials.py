ba18575b02ebf5403fc1e9bc984f744d2b13fb69
Haokun Liu <haokunliu412@gmail.com>
2020-03-06 14:36:54 -0500

    debug

--- scripts/taskmaster/optuna_hp_search/collect_trials.py
+++ scripts/taskmaster/optuna_hp_search/collect_trials.py
@@ -6,11 +6,13 @@ def collect_trails(study_name):
     storage = "sqlite:///example.db"
     study = optuna.load_study(study_name=study_name, storage=storage)
     df = study.trials_dataframe()
-    df = df.groupby(["user_attrs_batch_size", "user_attrs_lr", "user_attrs_max_epochs"])[
-        "performance"
+    print(df)
+    df_avg = df.groupby(["user_attrs_batch_size", "user_attrs_lr", "user_attrs_max_epochs"])[
+        "value"
     ].mean()
+    print(df_avg)
     csv_file = f"optuna_{study_name}.csv"
-    df.to_csv(csv_file, index=False)
+    df_avg.to_csv(csv_file, index=False)
 
 
 if __name__ == "__main__":
