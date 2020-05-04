24f60269b0488c202846f9d69532659ca5de9a44
Phil Yeres <6176602+pyeres@users.noreply.github.com>
2020-05-04 19:50:04 -0400

    mock jsonnet import for doc builder

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,8 @@ extensions = [
     "sphinx.ext.napoleon",
 ]
 
+autodoc_mock_imports = ["_tkinter"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
