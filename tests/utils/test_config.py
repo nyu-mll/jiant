ca0a2bb124c32e89655a0aee3807c539d1b90ba4
Phil Yeres <6176602+pyeres@users.noreply.github.com>
2020-05-04 17:15:52 -0400

    Add multi json document merge

--- tests/utils/test_config.py
+++ tests/utils/test_config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
+import os
+
 from jiant.utils import config
 
 
@@ -41,3 +43,18 @@ def test_json_merge_patch():
     merged_sorted: str = json.dumps(json.loads(merged), sort_keys=True)
     expected_sorted: str = json.dumps(json.loads(expected), sort_keys=True)
     assert merged_sorted == expected_sorted
+
+
+def test_merging_multiple_json_configs():
+    with open(os.path.join(os.path.dirname(__file__), "config/base_config.json")) as f:
+        base_config = f.read()
+    with open(os.path.join(os.path.dirname(__file__), "./config/first_override_config.json")) as f:
+        override_config_1 = f.read()
+    with open(os.path.join(os.path.dirname(__file__), "./config/second_override_config.json")) as f:
+        override_config_2 = f.read()
+    merged_config = config.merge_jsons_in_order([base_config, override_config_1, override_config_2])
+    with open(os.path.join(os.path.dirname(__file__), "./config/final_config.json")) as f:
+        expected_config = f.read()
+    sorted_merged_config = json.dumps(json.loads(merged_config), sort_keys=True)
+    sorted_expected_config = json.dumps(json.loads(expected_config), sort_keys=True)
+    assert sorted_merged_config == sorted_expected_config
