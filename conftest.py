86afde76db80d9593627afa90b4c359de499c1e2
Phil Yeres <6176602+pyeres@users.noreply.github.com>
2020-05-07 12:03:33 -0400

    Add pytest marker for skipping tests that require a GPU (#10)

--- conftest.py
+++ conftest.py
@@ -7,17 +7,22 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
+    parser.addoption("--rungpu", action="store_true", default=False, help="run gpu tests")
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "gpu: mark test as gpu required to run")
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+    if not config.getoption("--runslow"):
+        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
+    if not config.getoption("--rungpu"):
+        skip_gpu = pytest.mark.skip(reason="need --rungpu option to run")
+        for item in items:
+            if "gpu" in item.keywords:
+                item.add_marker(skip_gpu)
