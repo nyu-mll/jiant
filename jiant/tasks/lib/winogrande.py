2e81decf01b51143bdb73c30d0cd705fb61f1910
wh629 <60375376+wh629@users.noreply.github.com>
2020-09-21 21:23:07 -0400

    overwrote tokenized example

--- jiant/tasks/lib/winogrande.py
+++ jiant/tasks/lib/winogrande.py
@@ -14,7 +14,66 @@ class Example(mc_template.Example):
 
 @dataclass
 class TokenizedExample(mc_template.TokenizedExample):
-    pass
+    guid: str
+    prompt: List
+    choice_list: List[List]
+    label_id: int
+
+    def featurize(self, tokenizer, feat_spec):
+
+        if feat_spec.sep_token_extra:
+            maybe_extra_sep = [tokenizer.sep_token]
+            maybe_extra_sep_segment_id = [feat_spec.sequence_a_segment_id]
+            special_tokens_count = 4  # CLS, SEP-SEP, SEP
+        else:
+            maybe_extra_sep = []
+            maybe_extra_sep_segment_id = []
+            special_tokens_count = 3  # CLS, SEP, SEP
+
+        input_set_ls = []
+        unpadded_inputs_ls = []
+        for choice in self.choice_list:
+            prompt, choice = truncate_sequences(
+                tokens_ls=[self.prompt, choice],
+                max_length=feat_spec.max_seq_length - special_tokens_count,
+            )
+            unpadded_inputs = add_cls_token(
+                unpadded_tokens=(
+                    # choice
+                        choice
+                        + [tokenizer.sep_token]
+                        + maybe_extra_sep
+                        # prompt
+                        + prompt
+                        + [tokenizer.sep_token]
+                ),
+                unpadded_segment_ids=(
+                    # choice
+                        [feat_spec.sequence_a_segment_id] * (len(choice) + 1)
+                        + maybe_extra_sep_segment_id
+                        # prompt + sep
+                        + [feat_spec.sequence_b_segment_id] * (len(prompt) + 1)
+                ),
+                tokenizer=tokenizer,
+                feat_spec=feat_spec,
+            )
+            input_set = create_input_set_from_tokens_and_segments(
+                unpadded_tokens=unpadded_inputs.unpadded_tokens,
+                unpadded_segment_ids=unpadded_inputs.unpadded_segment_ids,
+                tokenizer=tokenizer,
+                feat_spec=feat_spec,
+            )
+            input_set_ls.append(input_set)
+            unpadded_inputs_ls.append(unpadded_inputs)
+
+        return DataRow(
+            guid=self.guid,
+            input_ids=np.stack([input_set.input_ids for input_set in input_set_ls]),
+            input_mask=np.stack([input_set.input_mask for input_set in input_set_ls]),
+            segment_ids=np.stack([input_set.segment_ids for input_set in input_set_ls]),
+            label_id=self.label_id,
+            tokens_list=[unpadded_inputs.unpadded_tokens for unpadded_inputs in unpadded_inputs_ls],
+        )
 
 
 @dataclass
