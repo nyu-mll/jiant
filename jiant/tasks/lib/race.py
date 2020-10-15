0d0e63463d4c15896ce1d485400062a458d1c6b8
Jason Phang <email@jasonphang.com>
2020-10-15 00:41:17 -0400

    race update

--- jiant/tasks/lib/race.py
+++ jiant/tasks/lib/race.py
@@ -1,10 +1,16 @@
 import glob
 import os
+import numpy as np
 
 from dataclasses import dataclass
 
-from jiant.tasks.lib.templates.shared import labels_to_bimap
+from jiant.tasks.lib.templates.shared import (
+    labels_to_bimap,
+    create_input_set_from_tokens_and_segments,
+    add_cls_token,
+)
 from jiant.tasks.lib.templates import multiple_choice as mc_template
+from jiant.tasks.utils import truncate_sequences
 from jiant.utils.python.io import read_json
 
 
@@ -14,10 +20,74 @@ class Example(mc_template.Example):
     def task(self):
         return RaceTask
 
+    def tokenize(self, tokenizer):
+        return TokenizedExample(
+            guid=self.guid,
+            prompt=tokenizer.tokenize(self.prompt),
+            choice_list=[tokenizer.tokenize(choice) for choice in self.choice_list],
+            label_id=self.task.CHOICE_TO_ID[self.label],
+        )
+
 
 @dataclass
 class TokenizedExample(mc_template.TokenizedExample):
-    pass
+    def featurize(self, tokenizer, feat_spec):
+        print("hi")
+
+        if feat_spec.sep_token_extra:
+            maybe_extra_sep = [tokenizer.sep_token]
+            maybe_extra_sep_segment_id = [feat_spec.sequence_a_segment_id]
+            special_tokens_count = 4  # CLS, SEP-SEP, SEP
+        else:
+            maybe_extra_sep = []
+            maybe_extra_sep_segment_id = []
+            special_tokens_count = 3  # CLS, SEP, SEP
+
+        input_set_ls = []
+        unpadded_inputs_ls = []
+        for choice in self.choice_list:
+            prompt, choice = truncate_sequences(
+                tokens_ls=[self.prompt, choice],
+                max_length=feat_spec.max_seq_length - special_tokens_count,
+                truncate_end=False,
+            )
+            unpadded_inputs = add_cls_token(
+                unpadded_tokens=(
+                    # prompt
+                    prompt
+                    + [tokenizer.sep_token]
+                    + maybe_extra_sep
+                    # choice
+                    + choice
+                    + [tokenizer.sep_token]
+                ),
+                unpadded_segment_ids=(
+                    # prompt
+                    [feat_spec.sequence_a_segment_id] * (len(prompt) + 1)
+                    + maybe_extra_sep_segment_id
+                    # choice + sep
+                    + [feat_spec.sequence_b_segment_id] * (len(choice) + 1)
+                ),
+                tokenizer=tokenizer,
+                feat_spec=feat_spec,
+            )
+            input_set = create_input_set_from_tokens_and_segments(
+                unpadded_tokens=unpadded_inputs.unpadded_tokens,
+                unpadded_segment_ids=unpadded_inputs.unpadded_segment_ids,
+                tokenizer=tokenizer,
+                feat_spec=feat_spec,
+            )
+            input_set_ls.append(input_set)
+            unpadded_inputs_ls.append(unpadded_inputs)
+
+        return DataRow(
+            guid=self.guid,
+            input_ids=np.stack([input_set.input_ids for input_set in input_set_ls]),
+            input_mask=np.stack([input_set.input_mask for input_set in input_set_ls]),
+            segment_ids=np.stack([input_set.segment_ids for input_set in input_set_ls]),
+            label_id=self.label_id,
+            tokens_list=[unpadded_inputs.unpadded_tokens for unpadded_inputs in unpadded_inputs_ls],
+        )
 
 
 @dataclass
@@ -32,7 +102,7 @@ class Batch(mc_template.Batch):
 
 class RaceTask(mc_template.AbstractMultipleChoiceTask):
     Example = Example
-    TokenizedExample = Example
+    TokenizedExample = TokenizedExample
     DataRow = DataRow
     Batch = Batch
 
