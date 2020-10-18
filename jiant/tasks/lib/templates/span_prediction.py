912ab3d1bf1b63faffdd76b4921ae066d018f85f
Phu Mon Htut--email=phumon.htut@nyu.edu <pmh330@gv02.nyu.cluster>
2020-10-18 16:58:25 -0400

    update

--- jiant/tasks/lib/templates/span_prediction.py
+++ jiant/tasks/lib/templates/span_prediction.py
@@ -5,6 +5,7 @@ import torch
 from dataclasses import dataclass
 from typing import List, Tuple
 
+from jiant.shared.model_resolution import resolve_is_lower_case
 from jiant.tasks.core import (
     Task,
     TaskTypes,
@@ -31,9 +32,11 @@ class Example(BaseExample):
     answer_char_span: (int, int)
 
     def tokenize(self, tokenizer):
+        passage = self.passage.lower() if tokenizer.do_lower_case else self.passage
+        passage_tokens = tokenizer.tokenize(passage)
 
-        passage_tokens = tokenizer.tokenize(self.passage)
-        token_aligner = TokenAligner(source=self.passage, target=passage_tokens)
+        token_aligner = TokenAligner(source=passage, target=passage_tokens)
+
         answer_token_span = token_aligner.project_char_to_token_span(
             self.answer_char_span[0], self.answer_char_span[1], inclusive=True
         )
@@ -43,7 +46,7 @@ class Example(BaseExample):
             passage=passage_tokens,
             question=tokenizer.tokenize(self.question),
             answer_str=self.answer,
-            passage_str=self.passage,
+            passage_str=passage,
             answer_token_span=answer_token_span,
             token_idx_to_char_idx_map=token_aligner.source_char_idx_to_target_token_idx.T,
         )
