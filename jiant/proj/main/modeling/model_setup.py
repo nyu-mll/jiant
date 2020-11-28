8fc9c8c954c0c9a587514b53068ff8c7b24c7c29
Jason Phang <email@jasonphang.com>
2020-11-28 04:55:09 -0500

    load encoder_only

--- jiant/proj/main/modeling/model_setup.py
+++ jiant/proj/main/modeling/model_setup.py
@@ -214,10 +214,10 @@ def load_encoder_only(jiant_model, weights_dict):
     # 2. Handle taskmodel encoders:
     for taskmodel_key in jiant_model.task_to_taskmodel_map:
         for encoder_key in encoder_keys:
-            new_key = f"taskmodels.{taskmodel_key}.encoder.{encoder_key}"
+            new_key = f"taskmodels_dict.{taskmodel_key}.encoder.{encoder_key}"
             new_weights_dict[new_key] = weights_dict[new_key]
 
-    jiant_model.load_state_dict(new_weights_dict, strict=False)
+    mismatch = jiant_model.load_state_dict(new_weights_dict, strict=False)
     return
 
 
