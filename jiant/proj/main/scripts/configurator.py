130ba066ae4ee6c7db5bfd899a2bb4e6624c40dc
Harsh Trivedi <harshjtrivedi94@gmail.com>
2020-12-24 14:14:52 -0500

    fix mistake in effective-batch-size for multigpu

--- jiant/proj/main/scripts/configurator.py
+++ jiant/proj/main/scripts/configurator.py
@@ -362,13 +362,7 @@ class SimpleAPIMultiTaskConfigurator(zconf.RunConfig):
         else:
             max_steps = self.max_steps
         for task_name in task_name_list_dict["train"]:
-            if self.num_gpus:
-                # We multiply by num_gpus because 1 step is done across (potentially) multiple GPUs
-                effective_batch_size = (
-                    self.train_batch_size * self.gradient_accumulation_steps * self.num_gpus
-                )
-            else:
-                effective_batch_size = self.train_batch_size * self.gradient_accumulation_steps
+            effective_batch_size = self.train_batch_size * self.gradient_accumulation_steps
             num_examples = get_num_examples_from_cache(
                 cache_path=os.path.expandvars(task_cache_config_dict[task_name]["train"]),
             )
