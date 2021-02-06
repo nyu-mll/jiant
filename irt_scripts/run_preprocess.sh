7b17c257f6cff7852a08ea97c6830e77aad4c9a1
Jason Phang <email@jasonphang.com>
2021-02-05 23:29:55 -0500

    irt update to new model api

--- irt_scripts/run_preprocess.sh
+++ irt_scripts/run_preprocess.sh
@@ -24,26 +24,22 @@ function prepare_all_tasks() {
 
 
 function preprocess_task(){
+    # Full model name e.g. nyu-mll/roberta-base-100M-1, roberta-base
     MODEL_TYPE=$1
     TASK_NAME=$2
 
-    if [[ $MODEL_TYPE = nyu* ]]
-    then
-        MODEL_NAME="${MODEL_TYPE##*/}"
-    else
-        MODEL_NAME=${MODEL_TYPE}
-    fi
-    echo "$MODEL_NAME: ${TASK_NAME}, ${DATA_DIR}"
+    # Model name e.g. roberta-base-100M-1, roberta-base
+    SHORT_MODEL_NAME="${MODEL_TYPE##*/}"
+    echo "${SHORT_MODEL_NAME}: ${TASK_NAME}, ${DATA_DIR}"
 
     python ${JIANT_PATH}/proj/main/tokenize_and_cache.py \
-	    --task_config_path ${DATA_DIR}/configs/${TASK_NAME}_config.json \
-            --model_type ${MODEL_NAME} \
-            --model_tokenizer_path ${MODELS_DIR}/${MODEL_NAME}/tokenizer \
-            --phases train,val,test \
-            --max_seq_length 256 \
-	    --do_iter \
-            --smart_truncate \
-	    --output_dir ${CACHE_DIR}/${MODEL_NAME}/${TASK_NAME}
+        --task_config_path ${DATA_DIR}/configs/${TASK_NAME}_config.json \
+        --hf_pretrained_model_name_or_path ${MODEL_TYPE} \
+        --phases train,val,test \
+        --max_seq_length 256 \
+        --do_iter \
+        --smart_truncate \
+        --output_dir ${CACHE_DIR}/${SHORT_MODEL_NAME}/${TASK_NAME}
 }
 
 
