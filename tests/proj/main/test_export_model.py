9f587ce7fc2576b1b0ff577b4e423c1ca1111748
Jesse Swanson <js11133@nyu.edu>
2021-04-08 11:47:39 -0400

    mark deberta test_export as slow

--- tests/proj/main/test_export_model.py
+++ tests/proj/main/test_export_model.py
@@ -17,8 +17,23 @@ from jiant.proj.main.export_model import export_model
     [
         ("bert", BertPreTrainedModel, "bert-base-cased"),
         ("roberta", RobertaForMaskedLM, "nyu-mll/roberta-med-small-1M-1",),
-        ("deberta-v2", DebertaV2ForMaskedLM, "microsoft/deberta-v2-xlarge",),
     ],
+)
+def test_export_model(tmp_path, model_type, model_class, hf_pretrained_model_name_or_path):
+    export_model(
+        hf_pretrained_model_name_or_path=hf_pretrained_model_name_or_path,
+        output_base_path=tmp_path,
+    )
+    read_config = py_io.read_json(os.path.join(tmp_path, f"config.json"))
+    assert read_config["model_type"] == model_type
+    assert read_config["model_path"] == os.path.join(tmp_path, "model", f"{model_type}.p")
+    assert read_config["model_config_path"] == os.path.join(tmp_path, "model", f"{model_type}.json")
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize(
+    "model_type, model_class, hf_pretrained_model_name_or_path",
+    [("deberta-v2", DebertaV2ForMaskedLM, "microsoft/deberta-v2-xlarge",),],
 )
 def test_export_model(tmp_path, model_type, model_class, hf_pretrained_model_name_or_path):
     export_model(
