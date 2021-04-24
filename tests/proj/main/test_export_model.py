b03f47cc780f95a6ba2341998d3cef17512a3ee3
Jesse Swanson <js11133Wnyu.edu>
2021-04-24 12:55:57 -0400

    fix test_expor_model tests

--- tests/proj/main/test_export_model.py
+++ tests/proj/main/test_export_model.py
@@ -25,22 +25,22 @@ def test_export_model(tmp_path, model_type, model_class, hf_pretrained_model_nam
         output_base_path=tmp_path,
     )
     read_config = py_io.read_json(os.path.join(tmp_path, f"config.json"))
-    assert read_config["model_type"] == model_type
-    assert read_config["model_path"] == os.path.join(tmp_path, "model", f"{model_type}.p")
-    assert read_config["model_config_path"] == os.path.join(tmp_path, "model", f"{model_type}.json")
+    assert read_config["hf_pretrained_model_name_or_path"] == hf_pretrained_model_name_or_path
+    assert read_config["model_path"] == os.path.join(tmp_path, "model", "model.p")
+    assert read_config["model_config_path"] == os.path.join(tmp_path, "model", "config.json")
 
 
 @pytest.mark.slow
 @pytest.mark.parametrize(
     "model_type, model_class, hf_pretrained_model_name_or_path",
-    [("deberta-v2", DebertaV2ForMaskedLM, "microsoft/deberta-v2-xlarge",),],
+    [("deberta-v2-xlarge", DebertaV2ForMaskedLM, "microsoft/deberta-v2-xlarge",), ],
 )
-def test_export_model(tmp_path, model_type, model_class, hf_pretrained_model_name_or_path):
+def test_export_model_large(tmp_path, model_type, model_class, hf_pretrained_model_name_or_path):
     export_model(
         hf_pretrained_model_name_or_path=hf_pretrained_model_name_or_path,
         output_base_path=tmp_path,
     )
     read_config = py_io.read_json(os.path.join(tmp_path, f"config.json"))
-    assert read_config["model_type"] == model_type
-    assert read_config["model_path"] == os.path.join(tmp_path, "model", f"{model_type}.p")
-    assert read_config["model_config_path"] == os.path.join(tmp_path, "model", f"{model_type}.json")
+    assert read_config["hf_pretrained_model_name_or_path"] == hf_pretrained_model_name_or_path
+    assert read_config["model_path"] == os.path.join(tmp_path, "model", "model.p")
+    assert read_config["model_config_path"] == os.path.join(tmp_path, "model", "config.json")
